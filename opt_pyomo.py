166fe0d17331583debcd47711a10af18ff96aa5c
colives <carlos.olives.camps@gmail.com>
2023-03-07 09:30:09 +0100

    Volume equations added

--- opt_pyomo.py
+++ opt_pyomo.py
@@ -5,19 +5,26 @@ Created on Tue Feb 14 16:29:21 2023
 import pyomo.environ as pyo
 
 rho_g = 1e3*9.81
+Dt = 1.
 A = 900
 B = 6
 Ho = 305
 K = 0.105
 cte_v = 100
-Q_tot = 4
+Q_tot = 10.
+W_0 = 35.
+W_max = 60.
+W_min = 30.
+P_max = 6*3e6
 
 model = pyo.AbstractModel()
 
-l_cost = (1, 5, )
-l_t = tuple(range(len(l_cost)))
-model.t = pyo.Set(initialize=l_t)
+q_out =  (1, 4, 2, 2, 3, 2, )
+l_cost = (1, 5, 8, 3, 9, 0, )
+n = len(l_cost)
+l_t = tuple(range(n))
 
+model.t = pyo.Set(initialize=l_t)
 model.cost_elec = pyo.Param(model.t, initialize=l_cost)
 
 model.rho_g = pyo.Param(initialize=rho_g)
@@ -27,11 +34,15 @@ model.Ho    = pyo.Param(initialize=Ho)
 model.K     = pyo.Param(initialize=K)
 model.cte_v = pyo.Param(initialize=cte_v)
 model.Q_tot = pyo.Param(initialize=Q_tot)
+model.W_0   = pyo.Param(initialize=W_0)
+model.Dt    = pyo.Param(initialize=Dt)
 
-model.P = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(0, 6*3e6)) # , initialize={0:4e6, 1:6e6}
-model.Q = pyo.Var(model.t, within=pyo.NonNegativeReals) #, initialize={0:6, 1:3}
-model.H = pyo.Var(model.t, within=pyo.NonNegativeReals) #, initialize={0:200, 1:200}
-model.a = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(0, 1)) # , initialize={0:0.5, 1:0.5}
+model.P = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, P_max),  initialize={k:4e6 for k in range(n)},)
+model.Q = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(None, None),   initialize={k:2   for k in range(n)},)
+model.H = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(None, None),   initialize={k:800 for k in range(n)},)
+model.a = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(0, 1),         initialize={k:0.5 for k in range(n)},)
+model.b = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1, None),      initialize={k:2.0 for k in range(n)},)
+model.W = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(W_min, W_max), initialize={k:W_0 for k in range(n)},)
 
 def Constraint_P(m, t):
     return m.P[t] == m.rho_g * m.Q[t] * m.H[t]
@@ -42,12 +53,23 @@ def Constraint_H(m, t):
 model.Constraint_H = pyo.Constraint(model.t, rule=Constraint_H)
 
 def Constraint_Hr_Hb(m, t):
-    return m.Ho + (m.K + m.cte_v/m.a[t])*(m.Q[t])**2 == m.A - m.B*(m.Q[t])**2
+    return m.Ho + (m.K + m.cte_v*m.b[t])*m.Q[t]**2 == m.A - m.B*m.Q[t]**2
 model.Constraint_Hr_Hb = pyo.Constraint(model.t, rule=Constraint_Hr_Hb)
 
-def Constraint_Q_total(m, t):
-    return sum(m.Q[t] for t in l_t) == m.Q_tot
-model.Constraint_Q_total = pyo.Constraint(model.t, rule=Constraint_Q_total)
+# def Constraint_Q_total(m, t):
+#     return sum(m.Q[t] for t in l_t) == m.Q_tot
+# model.Constraint_Q_total = pyo.Constraint(model.t, rule=Constraint_Q_total)
+
+def Constraint_dummy(m, t):
+    return m.a[t]*m.b[t] == 1.
+model.Constraint_dummy = pyo.Constraint(model.t, rule=Constraint_dummy)
+
+def Constraint_W_stored(m, t):
+    try:
+        return m.W[t] == m.W[t - 1] + m.Dt * (m.Q[t] - q_out[t])
+    except KeyError:
+        return m.W[t] == m.W_0 + m.Dt * (m.Q[t] - q_out[t])
+model.Constr_WSt = pyo.Constraint(model.t, rule=Constraint_W_stored)
 
 
 def obj_fun(m):
@@ -56,12 +78,11 @@ model.goal = pyo.Objective(rule=obj_fun, sense=pyo.minimize)
 
 
 instance = model.create_instance()
-solver = pyo.SolverFactory('baron')
-# solver.options['max_iter']= 10000
-results = solver.solve(instance)
-results.write()
+solver = pyo.SolverFactory('mindtpy')
+solver.solve(instance, mip_solver='glpk', nlp_solver='ipopt', tee=True)
 
 print(f'Power:  {instance.P.get_values()}')
 print(f'Flow:   {instance.Q.get_values()}')
 print(f'Height: {instance.H.get_values()}')
 print(f'Open:   {instance.a.get_values()}')
+print(f'Volume: {instance.W.get_values()}')
--- opt_scipy.py
+++ opt_scipy.py
@@ -5,44 +5,52 @@ Created on Tue Feb 14 15:36:19 2023
 
 import numpy as np
 from scipy.optimize import minimize
+from scipy.sparse import diags
 
 class problem():
 
-    def __init__(self, cost):
+    def __init__(self, cost, Dt):
+        self.Dt = Dt
         self.rho_g = 1e3*9.81
-        self.A = 900
-        self.B = 6
-        self.Ho = 305
+        self.A = 900.
+        self.B = 6.
+        self.Ho = 305.
         self.K = 0.105
         self.cte_v = 100.
+        self.W_0 = 35.
+        self.W_max = 60.
+        self.W_min = 12.
         self.p = np.ones_like(cost)*15e6
-        self.Q = np.ones_like(cost)*2
-        self.H = np.ones_like(cost)*800
-        self.a = np.ones_like(cost)*0.5
-        self.b = np.ones_like(cost)
-        self.x = np.concatenate((self.p, self.Q, self.H, self.b))
+        self.Q = np.ones_like(cost)*2.
+        self.H = np.ones_like(cost)*800.
+        self.a = np.ones_like(cost)
         self.P_max = 6*3e6
         self.Q_max = None
-        self.Q_tot = 4
+        self.Q_tot = 6.
         self.cost = cost
+        self.W = np.ones(len(cost)+1)
+        DIAGS = [self.W, -self.W[1:]]
+        self.M = diags(DIAGS, [0,-1]).toarray()
+        self.x = np.concatenate((self.p, self.Q, self.H, self.a, self.W))
 
 
     def optimization(self):
         n = len(self.cost)
-        fun = lambda x: np.dot(self.cost, x[0:n])
+        fun = lambda x: self.Dt*np.dot(self.cost, x[0:n])
         cons = [{'type':'eq', 'fun': lambda x: x[2*n:3*n] - self.A + self.B * x[n:2*n]**2},
                 {'type':'eq', 'fun': lambda x: self.A - self.B * x[n:2*n]**2 - self.Ho - (self.K + self.cte_v * x[3*n:4*n])*x[n:2*n]**2},
                 {'type':'eq', 'fun': lambda x: x[0:n] - self.rho_g * x[n:2*n]*x[2*n:3*n]},
-                {'type':'eq', 'fun': lambda x: sum(x[n:2*n]) - self.Q_tot},]
-        #bnds = tuple([(0, None)]*3*n + [(1e-6, 1)]*n)
-        bnds = tuple([(0, self.P_max)]*n + [(0, self.Q_max)]*n + [(0, None)]*n + [(1, None)]*n)
-        res = minimize(fun, self.x, method='SLSQP', constraints=cons, bounds=bnds, tol = 1e-4)
-        print(res.message)
+                {'type':'eq', 'fun': lambda x: sum(x[n:2*n]) - self.Q_tot},
+                {'type':'eq', 'fun': lambda x: np.dot(self.M, x[4*n:]) - self.Dt*np.concatenate((np.array([self.W_0]), x[n:2*n]))},]
+        bnds = tuple([(0, self.P_max)]*n + [(0, self.Q_max)]*n + [(0, None)]*n + [(1, None)]*n + [(self.W_min, self.W_max)]*(n+1))
+        res = minimize(fun, self.x, method='SLSQP', constraints=cons, bounds=bnds, tol = 1e-4, options={'maxiter':300, 'disp':True})
         self.x = res.x
 
 
-cost = np.array([1, 5,])
+cost = np.array([1, 5, 8, 3])
+Dt = 1
 n = len(cost)
-prb = problem(cost)
+prb = problem(cost, Dt)
 prb.optimization()
-print(f'power:  {prb.x[:n]} \nflow:   {prb.x[n:2*n]} \nheight: {prb.x[2*n:3*n]} \nvalv:   {1/prb.x[3*n:]} \n')
+print(f'power:  {prb.x[:n]} \nflow:   {prb.x[n:2*n]} \nheight: {prb.x[2*n:3*n]} \nvalv:   {1/prb.x[3*n:4*n]}  \nvolm:   {prb.x[4*n:]}')
+
