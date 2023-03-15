5e8efc835bc98ff3f287a6d01923937e766f2332
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-03-15 09:26:51 +0100

    <- this file should be removed

--- proves_scipy.py
+++ proves_scipy.py
@@ -59,27 +59,104 @@ import pyomo.core as pyo
 from pyomo.opt import SolverFactory
 import numpy as np
 
+
+# CONSTANTS
 rho_g = 1e3*9.81
+
+
+# DEFINITIONS
+class Pump:
+    
+    def __init__(self, A, B, Pmax=1e6):
+        self.A = A
+        self.B = B
+        self.Pmax = Pmax
+        
+    def H(self, Q):
+        return self.A + self.B*(Q**2)
+
+class Pipe:
+    
+    def __init__(self, Ho, K, Kv, pumps=[]):
+        self.Ho = Ho
+        self.K = K
+        self.Kv = Kv
+        self.pumps = pumps
+        
+    def add_pump(A, B, Pmax):
+        self.pumps = self.pumps.append((A,B,Pmax))
+        
+    def H(self, Q, var_a):
+        return self.Ho + (self.K + self.Kv/a)*(Q**2)
+
+
+class Reservoir:
+    
+    def __init__(self, W_o, W_total, W_min, W_max, syst_in, syst_out):
+        self.W_total = W_total
+        self.W_min = W_min
+        self.W_max = W_max
+        self.W = W_o
+        self.syst_in = syst_in
+        self.syst_out = syst_out
+        
+        
+    def W(self, dt=3600):
+        Q_in = 
+        Q_out = 
+        self.W = self.W + (Q_in - Q_out)*dt
+        return self.W
+
+
+model = pyo.AbstractModel()
+
+
+# SETS
+l_t = list(range(24))
+model.t = pyo.Set(initialize=l_t)
+
+list_pumps = [Pump(900,6), Pump(900,6)]
+list_syst = [System(305,0.105,100,[list_pumps[0]]),
+             System(305,0.105,100,[list_pumps[1]])]
+
+list_reservoirs = [Reservoir(1e10,0.01,0.99, [], list_syst[0]), 
+                   Reservoir(100,0.3,0.7, list_syst[0], list_syst[1]), 
+                   Reservoir(100,0.3,0.7, list_syst[1], [])]
+model.r = pyo.Set(initialize=list_reservoirs)
+
+
+# PARAMS
+list_cost = [-(x-12)**2+144 for x in range(24)]
+model.cost = pyo.Param(initialize=list_cost)
+
+
 A = 900
 B = 6
 Ho = 305
 K = 0.105
 cte_v = 100
 
-Q_total = 4
+Q_total = 10
 
-model = pyo.AbstractModel()
 
-l_t = list(range(2))
-model.t = pyo.Set(initialize=l_t)
 
-l_cost = [1, 5]
+l_cost = [1, 5, 10, 5, 1]
 model.cost_elec = pyo.Param(model.t, initialize=l_cost)
 
-model.P = pyo.Var(model.t, within=pyo.NonNegativeReals, initialize={0:4e6, 1:6e6})
-model.Q = pyo.Var(model.t, within=pyo.NonNegativeReals, initialize={0:6, 1:3})
-model.H = pyo.Var(model.t, within=pyo.NonNegativeReals, initialize={0:200, 1:200})
-model.a = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(0,1), initialize={0:0.5, 1:0.5})
+
+vals = [6e6]*len(l_t)
+model.P = pyo.Var(model.t, within=pyo.NonNegativeReals, initialize={ k:v for (k,v) in zip(l_t, vals)})
+
+vals = [1]*len(l_t)
+model.Q = pyo.Var(model.t, within=pyo.NonNegativeReals, initialize={ k:v for (k,v) in zip(l_t, vals)})
+
+vals = [400]*len(l_t)
+model.H = pyo.Var(model.t, within=pyo.NonNegativeReals, initialize={ k:v for (k,v) in zip(l_t, vals)})
+
+vals = [0.5]*len(l_t)
+model.a = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(0,1), initialize={ k:v for (k,v) in zip(l_t, vals)})
+
+
 
 def Constraint_P(m, t):
     return m.P[t] == rho_g * m.Q[t] * m.H[t]
@@ -93,9 +170,9 @@ def Constraint_Hr_Hb(m, t):
     return Ho+(K+cte_v/m.a[t])*(m.Q[t])**2 == A - B*(m.Q[t])**2
 model.Constraint_Hr_Hb = pyo.Constraint(model.t, rule=Constraint_Hr_Hb)
 
-# def Constraint_Q_max(m, t):
-#     return m.Q[t]<=9
-# model.Constraint_Q_max = pyo.Constraint(model.t, rule=Constraint_Q_max)
+def Constraint_Q_max(m, t):
+    return m.Q[t]<=10
+model.Constraint_Q_max = pyo.Constraint(model.t, rule=Constraint_Q_max)
 
 def Constraint_Q_total(m, t):
     return sum(m.Q[t] for t in l_t) == Q_total
@@ -109,18 +186,44 @@ model.goal = pyo.Objective(rule=obj_fun, sense=pyo.minimize)
 
 
 instance = model.create_instance()
-solver = SolverFactory('ipopt')
-solver.options['max_iter']= 10000
-results = solver.solve(instance)
-results.write()
+solver = SolverFactory('mindtpy') #ipopt
+# solver.options['max_iter']= 10000
+results = solver.solve(instance, tee=True)
+instance.pprint()
 
-instance.P.get_values()
-instance.Q.get_values()
-instance.H.get_values()
-instance.a.get_values()
+print('P: ', instance.P.get_values())
+print('Q: ', instance.Q.get_values())
+print('H: ', instance.H.get_values())
+print('a: ', instance.a.get_values())
 
 
+#%%
 
+import pyomo.environ
+import pyomo.core as pyo
+from pyomo.opt import SolverFactory
+import numpy as np
+
+# PRE PROCESSING
+n_basses = 4
+n_canonades = 3
+
+l_time = list(range(24))
+l_cost = (np.arange(24, )list(range(24))-12)**2
+
+l_W_top = [10, 10, 10, 10] #n_basses
+l_W_bot = [3, 3, 3, 3] #n_basses
+
+l_Ho = [266, 93, 23] #n_canonades
+l_K = [0.20, 0.20, 0.20] #n_canonades
+
+l_Pmax = [3*3.2e6, 2*250e3, 2*1.25e6] #n_canonades
+
+
+model = pyo.AbstractModel()
+
+### Model Sets ###
+model.t = pyo.Set(initialize=l_time)
 
 
 
