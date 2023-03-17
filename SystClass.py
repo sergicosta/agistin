d858f1db3d93049f95c3091fb58c01736285a2f2
colives <carlos.olives.camps@gmail.com>
2023-03-17 15:07:59 +0100

    More elements added

--- SystClass.py
+++ SystClass.py
@@ -4,8 +4,10 @@ Created on Tue Mar  7 09:32:33 2023
 
 Class system represents the whole system, define elements in the following order:
     1. Reservoirs
-    2. Pipes (connecting reservoirs)
-    3. Pumps (associated to pipes)
+    2. Pump Stations [EB] (electrical power balance)
+    3. Pipes (connecting reservoirs)
+    4. Pumps (associated to pipes)
+    5. PV, batteries ... linked to EBs
     
 """
 
@@ -13,27 +15,36 @@ class system():
     
     def __init__(self):
         self.rsvrs = dict()
+        self.EBs   = dict()
         self.pumps = dict()
         self.pipes = dict()
         self.pvs   = dict()
-        self.id_rs, self.id_pm, self.id_pp, self.id_pv = 0, 0, 0, 0
+        self.batts = dict()
+        self.trbs  = dict()
+        self.id_rs, self.id_eb, self.id_pm, self.id_pp, self.id_pv, self.id_bat, self.id_trb = 0, 0, 0, 0, 0, 0, 0
     
     def add_rsvr(self, W_0, W_max, W_min):
         self.rsvrs[f'{self.id_rs}'] = reservoir(self.id_rs, W_0, W_max, W_min)
         self.id_rs += 1
+        
+    def add_EB(self, p_trafo):
+        self.EBs[f'{self.id_eb}'] = EB(self.id_eb, p_trafo)
+        self.id_eb += 1
     
-    def add_pump(self, rho_g, A, B, p_max, Q_max, in_pipe):
-        self.pumps[f'{self.id_pm}'] = pump(self, self.id_pm, rho_g, A, B, p_max, Q_max, in_pipe)
+    def add_pump(self, rho_g, A, B, p_max, Q_max, in_pipe, eb_loc):
+        self.pumps[f'{self.id_pm}'] = pump(self, self.id_pm, rho_g, A, B, p_max, Q_max, in_pipe, eb_loc)
         if self.pumps[f'{self.id_pm}'].verification == False:
             del(self.pumps[f'{self.id_pm}'])
         else:
+            self.pumps[f'{self.id_pm}'].link_to()
             self.id_pm += 1
     
-    def add_pump_simple(self, p_max, Q_max, efficiency, in_pipe):
-        self.pumps[f'{self.id_pm}'] = pump_simple(self, self.id_pm, p_max, Q_max, efficiency, in_pipe)
+    def add_pump_simple(self, p_max, Q_max, efficiency, in_pipe, eb_loc):
+        self.pumps[f'{self.id_pm}'] = pump_simple(self, self.id_pm, p_max, Q_max, efficiency, in_pipe, eb_loc)
         if self.pumps[f'{self.id_pm}'].verification == False:
             del(self.pumps[f'{self.id_pm}'])
         else:
+            self.pumps[f'{self.id_pm}'].link_to()
             self.id_pm += 1
     
     def add_pipe(self, K_i, H_0, orig, end, valve=False, C_v=None, alpha=None):
@@ -41,12 +52,28 @@ class system():
         if self.pipes[f'{self.id_pp}'].verification == False:
             del(self.pipes[f'{self.id_pp}'])
         else:
-            self.pipes[f'{self.id_pp}'].vinculo(self, orig, end)
+            self.pipes[f'{self.id_pp}'].link_to()
             self.id_pp += 1
             
-    def add_PV(self, P_plant):
-        self.pvs[f'{self.id_pv}'] = PV(self, self.id_pv, P_plant)
-        self.id_pv += 1
+    def add_PV(self, EB_loc, p_plant):
+        self.pvs[f'{self.id_pv}'] = PV(self, self.id_pv, EB_loc,p_plant)
+        if self.pvs[f'{self.id_pv}'].verification == False:
+            del(self.pvs[f'{self.id_pv}'])
+        else:
+            self.pvs[f'{self.id_pv}'].link_to()
+            self.id_pv += 1
+        
+    def add_battery(self, EB_loc, p_bat):
+        self.batts[f'{self.id_bat}'] = battery(self, self.id_bat, EB_loc, p_bat)
+        if self.batts[f'{self.id_bat}'].verification == False:
+            del(self.batts[f'{self.id_bat}'])
+        else:
+            self.batts[f'{self.id_bat}'].link_to()
+            self.id_bat += 1
+            
+    
+    def add_turbine(self, p_max):
+        pass
             
     def builder(self): # , obj_fun
         file_name = 'optimization.py'
@@ -63,7 +90,7 @@ class system():
             
             vars_txt = ''
             eqs_txt = ''
-            for element_dict in [self.rsvrs, self.pumps, self.pipes]:
+            for element_dict in [self.rsvrs, self.EBs, self.pumps, self.pipes, self.pvs, self.batts, self.trbs]:
                 for ID in element_dict.keys():
                     for v in element_dict[ID].var: # write variables
                         vars_txt = vars_txt + v + '\n'
@@ -96,7 +123,6 @@ class reservoir(syst_element):
         self.W_min = W_min
         self.Q_in = list()
         self.Q_out = list()
-        self.eqs = list()
         self.var = ['model.'+self.x[0]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=('+str(self.W_min)+', '+str(self.W_max)+'), initialize={k:'+str(self.W_0)+' for k in range(n)},)',]
         
     def eq_write(self):
@@ -122,10 +148,36 @@ class reservoir(syst_element):
                         f'model.Constraint_{self.x[0]} = pyo.Constraint(model.t, rule=Constraint_{self.x[0]})')
 
 
+class EB(syst_element):
+    
+    def __init__(self, ID, p_max):
+        super().__init__(ID, [f'P_g{ID}'])
+        self.P_in = list() # PVs and turbines generated power
+        self.P_out = list() # pumps demanded power
+        self.P_bal = list() # batteries power
+        self.p_g_max = p_max
+        self.var = ['model.'+self.x[0]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=('+str(-self.p_g_max)+', '+str(self.p_g_max)+'),  initialize={k:0.0 for k in range(n)},)',]
+        
+    def eq_write(self):
+        p_in, p_out, p_bal = '','',''
+        if len(self.P_in)>0:
+            for p in self.P_in:
+                p_in += f' + m.{p}[t]'
+        if len(self.P_out)>0:
+            for p in self.P_out:
+                p_out += f' + m.{p}[t]'
+        if len(self.P_bal)>0:
+            for p in self.P_bal:
+                p_bal += f' + m.{p}[t]'
+        self.eqs.append(f'def Constraint_{self.x[0]}(m, t): \n'
+                        f'\treturn m.{self.x[0]}[t] == ('+p_bal+') + ('+p_in+') - ('+p_out+')\n'
+                        f'model.Constraint_{self.x[0]} = pyo.Constraint(model.t, rule=Constraint_{self.x[0]})')
+
+
 class pump(syst_element):
     
-    def __init__(self, system, ID, rho_g, A, B, p_max, Q_max, rpm_nominal, in_pipe):
-        super().__init__(ID, [f'p_b{ID}', f'Q_b{ID}', f'H_b{ID}', f'rpm_{ID}'])
+    def __init__(self, system, ID, rho_g, A, B, p_max, Q_max, rpm_nominal, in_pipe, EB_loc):
+        super().__init__(ID, [f'p_b{ID}_h', f'Q_b{ID}', f'H_b{ID}', f'rpm_{ID}', f'p_b{ID}_e'])
         self.system = system
         self.rho_g = rho_g
         self.A = A
@@ -135,7 +187,7 @@ class pump(syst_element):
         self.rpm_nominal = rpm_nominal
         self.verification = True
         self.conn = self.conns(in_pipe)
-        self.eqs = list()
+        self.loc = EB_loc
         self.var = ['model.'+self.x[0]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, '+str(self.p_max)+'),  initialize={k:'+str(0.8*self.p_max)+' for k in range(n)},)',
                     'model.'+self.x[1]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, '+str(self.Q_max)+'),   initialize={k:'+str(0.8*self.Q_max)+'   for k in range(n)},)',
                     'model.'+self.x[2]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(None, None),   initialize={k:'+str(self.A)+' for k in range(n)},)',]
@@ -147,7 +199,10 @@ class pump(syst_element):
         else:
             print('Associated pipe ID does not match any created. Pump eliminated')
             self.verification *= False
-        
+    
+    def link_to(self):
+        self.system.EBs[f'{self.loc}'].P_out.append(self.x[0])
+    
     def eq_write(self):
         self.eqs.append(f'def Constraint_{self.x[0]}(m, t): \n'
                         f'\treturn m.{self.x[0]}[t] == {self.rho_g} * m.{self.x[1]}[t] * m.{self.x[2]}[t]'
@@ -159,7 +214,7 @@ class pump(syst_element):
     
 class pump_simple(syst_element):
     
-    def __init__(self, system, ID, p_max, Q_max, efficiency, in_pipe):
+    def __init__(self, system, ID, p_max, Q_max, efficiency, in_pipe, EB_loc):
         super().__init__(ID, [f'p_b{ID}', f'Q_b{ID}'])
         self.system = system
         self.p_max = p_max
@@ -167,7 +222,7 @@ class pump_simple(syst_element):
         self.efficiency = efficiency
         self.verification = True
         self.conn = self.conns(in_pipe)
-        self.eqs = list()
+        self.loc = EB_loc
         self.var = ['model.'+self.x[0]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, '+str(self.p_max)+'),  initialize={k:'+str(0.8*self.p_max)+' for k in range(n)},)',
                     'model.'+self.x[1]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, '+str(self.Q_max)+'),   initialize={k:'+str(0.8*self.Q_max)+'   for k in range(n)},)',
                     ]
@@ -179,13 +234,21 @@ class pump_simple(syst_element):
         else:
             print('Associated pipe ID does not match any created. Pump eliminated')
             self.verification *= False
+            
+    def link_to(self):
+        self.system.EBs[f'{self.loc}'].P_out.append(self.x[0])
         
     def eq_write(self):
-        
         self.eqs.append(f'def Constraint_{self.x[0]}(m, t): \n'
                         f'\treturn m.{self.x[0]}[t] == m.{self.x[1]}[t] / {self.efficiency}\n'
                         f'model.Constraint_{self.x[0]} = pyo.Constraint(model.t, rule=Constraint_{self.x[0]})')
 
+
+class turbine(syst_element):
+    
+    def __init__(self, system, ID, x):
+        super().__init__(ID, x)
+        
 
 class pipe(syst_element):
     
@@ -223,9 +286,9 @@ class pipe(syst_element):
             print('Output reservoir ID does not match any created. Pipe eliminated')
             self.verification *= False
             
-    def vinculo(self, sys, orig, end):
-        sys.rsvrs[f'{end}'].Q_in.append(self.x[0])
-        sys.rsvrs[f'{orig}'].Q_out.append(self.x[0])
+    def link_to(self):
+        self.system.rsvrs[f'{self.end}'].Q_in.append(self.x[0])
+        self.system.rsvrs[f'{self.orig}'].Q_out.append(self.x[0])
         
     def eq_write(self):
         if len(self.parallel_pumps)>0:
@@ -239,26 +302,80 @@ class pipe(syst_element):
     
 class PV(syst_element):
     
-    def __init__(self, system, ID, P_plant):
+    def __init__(self, system, ID, EB_loc, P_plant):
         super().__init__(ID, [f'P_gpv{ID}'])
+        self.system = system
+        self.eb_loc = self.conn(EB_loc)
         self.P_plant = P_plant
+        self.verification = True
         self.var = ['model.'+self.x[0]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, '+str(self.P_plant)+'),   initialize={k:'+str(0.5*self.P_plant)+'   for k in range(n)},)',]
+
+    def link_to(self):
+        self.system.EBs[f'{self.eb_loc}'].P_in.append(self.x[0])
+        
+    def conn(self, loc):
+        if str(loc) in list(self.system.EBs.keys()):
+            return loc
+        else:
+            print('Associated EB ID does not match any created. PV eliminated')
+            self.verification *= False
 
     def eq_write(self):
         self.eqs.append(f'def Constraint_{self.x[0]}(m, t):\n'
                         f'\treturn m.{self.x[0]}[t] <= P_pv{self.ID}[t]\n'
                         f'model.Constraint_{self.x[0]} = pyo.Constraint(model.t, rule=Constraint_{self.x[0]})')
         
+        
+class battery(syst_element):
+    
+    def __init__(self, system, ID, EB_loc, P_bat):
+        super().__init__(ID, [f'P_bat{ID}'])
+        self.system = system
+        self.eb_loc = self.conn(EB_loc)
+        self.P_bat = P_bat
+        self.verification = True
+        self.var = ['model.'+self.x[0]+' = pyo.Var(model.t, within=pyo.NonNegativeReals, bounds=(1e-6, '+str(self.P_bat)+'),   initialize={k:'+str(0.5*self.P_bat)+'   for k in range(n)},)',]
+    
+    def link_to(self):
+        self.system.EBs[f'{self.eb_loc}'].P_bal.append(self.x[0])
+    
+    def conn(self, loc):
+        if str(loc) in list(self.system.EBs.keys()):
+            return loc
+        else:
+            print('Associated EB ID does not match any created. Battery eliminated')
+            self.verification *= False
+            
+    def eq_write(self):
+        pass
+        
+        
 if __name__ == "__main__":
     
+    # System definition
     sgr_sud = system()
+    
+    # Reservoirs definition
     sgr_sud.add_rsvr(58, 90, 30)
     # sgr_sud.add_rsvr(34, 50, 20)
     sgr_sud.add_rsvr(5, 50, 20)
+    
+    # EBs definition
+    sgr_sud.add_EB(1e6)
+    
+    # Pipes definition
     sgr_sud.add_pipe(5, 630, 0, 1)
+    
+    # Pumps definition
     # sgr_sud.add_pump(9.81e3, 800, 12, 25e4, 1450, 2, 0)
-    sgr_sud.add_pump_simple(25e4, 2, 0.5, 0)
-    sgr_sud.add_pump_simple(25e4, 2, 0.9, 0)
+    sgr_sud.add_pump_simple(25e4, 2, 0.5, 0, 0)
+    sgr_sud.add_pump_simple(25e4, 2, 0.9, 0, 0)
+    
+    # PVs difinition
+    sgr_sud.add_PV(0, 5e2)
+    
+    # Batteries definition
+    sgr_sud.add_battery(0, 1e3)
     
     sgr_sud.builder()
     
@@ -275,8 +392,3 @@ if __name__ == "__main__":
     #     sgr_sud.pumps[i].eq_write()
     #     for k in sgr_sud.pumps[i].eqs:
     #         print(k)
-
-
-        
-        
-        
