76d613d417905e7d0057785628c206051a552242
colives <carlos.olives.camps@gmail.com>
2023-05-12 13:08:21 +0200

    solving synchro problem

--- SegriaSud.py
+++ SegriaSud.py
@@ -19,17 +19,18 @@ sgr_sud.add_rsvr(35, 50, 20)
 sgr_sud.add_EB(100e6)
 
 # Pipes definition
-sgr_sud.add_pipe(20, 50, 0, 1)
-sgr_sud.add_pipe(20, 50, 0, 1)
-sgr_sud.add_pipe(20, 50, 0, 1)
+sgr_sud.add_pipe(23, 5e-5, 0, 1)
+# sgr_sud.add_pipe(20, 50, 0, 1)
+# sgr_sud.add_pipe(20, 50, 0, 1)
 
 # Pumps definition
-sgr_sud.add_pump(80, 20, 25e6, 1450, 2000, 0, 0)
+# PDH-800-90-M (A)
+sgr_sud.add_pump(106.3, 3e-5, 272.13e3, 1035.21, 1480, 0.87, 0, 0)
 # sgr_sud.add_pump_simple(25e4, 50, 0.5, 0, 0)
 # sgr_sud.add_pump_simple(25e4, 2, 0.9, 0, 0)
-sgr_sud.add_turbine_simple(25e6, 2000, 0.5, 1, 0)
+# sgr_sud.add_turbine_simple(25e6, 2000, 0.5, 1, 0)
 # Pump to dim
-sgr_sud.add_new_pump(2, 0)
+# sgr_sud.add_new_pump(2, 0)
 
 # PVs definition
 sgr_sud.add_PV(0, 5e5, 1e4)
@@ -43,7 +44,7 @@ reslt = sgr_sud.run()
 print()
 print(f'Potencia xarxa EB0: {reslt["P_g0"]}')
 print(f'Potencia bomba: {reslt["Ph_b0"]}')
-print(f'Potencia turbina: {reslt["P_trb0"]}')
+# print(f'Potencia turbina: {reslt["P_trb0"]}')
 print(f'Potencia PV: {reslt["P_pv_g0"]}')
 
 # for i in sgr_sud.rsvrs.keys():
