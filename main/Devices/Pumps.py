cee5228a023cb7830bba049ac36f5609c1c469cc
colives <carlos.olives.camps@gmail.com>
2023-07-07 12:29:11 +0200

    pump updated

--- main/Devices/Pumps.py
+++ main/Devices/Pumps.py
@@ -2,64 +2,37 @@
 """
 AGISTIN project 
 
-...\Devices\PumpsClass.py
+.\Devices\PumpsClass.py
 
-Class Pumps contains characteristics of a pump.
+Pump pyomo block contains characteristics of a pump.
 """
 
-from Devices.Elements import Elements
+
 import pyomo.environ as pyo
+from pyomo.network import *
 
-class Pumps(Elements):
+def Pump(b, t, data, init_data):
     
-    def __init__(self):
-        super().__init__()
-        
-        self.A = {} # --> {id:val}
-        self.B = {}
-        self.rpm_nom = {}
-        
-        # init variables
-        self.init_H = {} # --> {(id,t):val}
-        self.init_Q = {} # --> {(id,t):val}
-        self.init_n = {} # --> {(id,t):val}
-        
-        
-    def add(self, id_elem, A, B, rpm_nom, init_H, init_Q, l_t, init_n=None):
-        super().add(id_elem)
-        
-        self.A[id_elem] = A
-        self.B[id_elem] = B
-        self.rpm_nom[id_elem] = rpm_nom
-        
-        # init variables
-        self.init_H.update({(id_elem,t): init_Q for t in l_t})
-        self.init_Q.update({(id_elem,t): init_H for t in l_t})
-        if init_n==None:
-            init_n = rpm_nom
-        self.init_n.update({(id_elem,t): init_n for t in l_t})
-        
-        
-    def builder(self, model):
-        model.i_pumps = pyo.Set(initialize=self.id)
-        # PARAMS
-        model.pumps_A = pyo.Param(model.i_pumps, initialize=self.A, within=pyo.NonNegativeReals)
-        model.pumps_B = pyo.Param(model.i_pumps, initialize=self.B, within=pyo.NonNegativeReals)
-        model.pumps_rpm_nom = pyo.Param(model.i_pumps, initialize=self.rpm_nom, within=pyo.NonNegativeReals)
-        # VARIABLES
-        model.pumps_H = pyo.Var(model.i_pumps, model.t, initialize=self.init_H, within=pyo.NonNegativeReals)
-        model.pumps_Q = pyo.Var(model.i_pumps, model.t, initialize=self.init_Q, within=pyo.NonNegativeReals)
-        model.pumps_n = pyo.Var(model.i_pumps, model.t, initialize=self.init_n, within=pyo.NonNegativeReals)
-        
-        
-    def builderConstr(self, model):
-        model.Constraint_Qmax_Pumps = pyo.Constraint(model.i_pumps, model.t, rule=Constraint_Qmax_Pumps)
-        model.Constraint_H_Pumps = pyo.Constraint(model.i_pumps, model.t, rule=Constraint_H_Pumps)
+    # Parameters
+    b.A = pyo.Param(initialize=data['A'])
+    b.B = pyo.Param(initialize=data['B'])
+    b.Pmax = pyo.Param(initialize=data['Pmax'])
+    b.Qmin = pyo.Param(initialize=data['Qmin'])
+    b.Qmax = pyo.Param(initialize=data['Qmax'])
+    b.Qnom = pyo.Param(initialize=data['Qnom'])
+    b.n_nom = pyo.Param(initialize=data['rpm_nom'])
+    b.nu_nom = pyo.Param(initialize=data['nu_nom'])
     
-# CONSTRAINTS
-
-def Constraint_Qmax_Pumps(m, i_pumps, t):
-    return m.pumps_Q[i_pumps, t] <= m.pumps_Qmax[i_pumps, t]
-
-def Constraint_H_Pumps(m, i_pumps, t): 
- 	return m.pumps_H[i_pumps, t] == ((m.pumps_n[i_pumps, t]/m.pumps_rpm_nom[i_pumps])**2) * m.pumps_A[i_pumps] - m.pumps_B[i_pumps]*(m.pumps_Q[i_pumps,t])**2
+    # Variables
+    b.P = pyo.Var(t, initialize=init_data['P'])#, bounds=(0.0, b.Pmax), domain=pyo.NonNegativeReals)
+    b.H = pyo.Var(t, initialize=init_data['H'], domain=pyo.NonNegativeReals)
+    b.Q = pyo.Var(t, initialize=init_data['Q'], bounds=(b.Qmin, b.Qmax), domain=pyo.NonNegativeReals)
+    b.n = pyo.Var(t, initialize=init_data['n'], domain=pyo.NonNegativeReals)
+    
+    # Ports
+    b.outlet = Port(initialize={'q': (b.Q, Port.Extensive)})
+    b.inlet = Port(initialize={'p': (b.P, Port.Extensive)})
+    
+    def Constraint_H_Pumps(_b, _t): 
+     	return _b.H[_t] == ((b.n[_t]/b.n_nom)**2)*b.A - b.B*(b.Q[_t])**2
+    b.Constraint_H_Pumps = pyo.Constraint(t, rule=Constraint_H_Pumps)
