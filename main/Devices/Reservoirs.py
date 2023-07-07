a9c1eadb86ceb3a2901ab53a3e00bf689d3a9998
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-07 16:38:03 +0200

    Added ports Qin and Qout to reservoirs

--- main/Devices/Reservoirs.py
+++ main/Devices/Reservoirs.py
@@ -20,16 +20,18 @@ def Reservoir(b, t, data, init_data):
     b.W0 = pyo.Param(initialize=data['W0'])
     
     # Variables
-    b.Q = pyo.Var(t, initialize=init_data['Q'], within=pyo.NonNegativeReals)
+    b.Qin = pyo.Var(t, initialize=init_data['Q'], within=pyo.NonNegativeReals)
+    b.Qout = pyo.Var(t, initialize=init_data['Q'], within=pyo.NonNegativeReals)
     b.W = pyo.Var(t, initialize=init_data['W'], bounds=(data['Wmin'], data['Wmax']), within=pyo.NonNegativeReals)
     
     # Ports
-    b.port_Q = Port(initialize={'Q': (b.Qin, Port.Extensive)})
+    b.port_Qin = Port(initialize={'Q': (b.Qin, Port.Extensive)})
+    b.port_Qout = Port(initialize={'Q': (b.Qout, Port.Extensive)})
 
     # Constraints
     def Constraint_W(_b, _t):
         if _t>0:
-            return _b.W[_t] == _b.W[_t-1] + _b.Q[_t] # TODO: - Qloss - gamma
+            return _b.W[_t] == _b.W[_t-1] + _b.Qin[_t] - _b.Qout[_t] # TODO: - Qloss - gamma
         else:
-            return _b.W[_t] == _b.W0 + _b.Q[_t]
+            return _b.W[_t] == _b.W0 + _b.Qin[_t] - _b.Qout[_t]
     b.c_W = pyo.Constraint(t, rule = Constraint_W)
