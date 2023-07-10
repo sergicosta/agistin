23512fa40d1719cd12eaaedf57ccf8d96800b003
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-10 11:43:08 +0200

    Fixed t indexes

--- main/Devices/MainGrid.py
+++ main/Devices/MainGrid.py
@@ -14,13 +14,13 @@ from pyomo.network import *
 # data: Pmax
 # init_data: None
 
-def grid(b, t, data, init_data):
+def Grid(b, t, data, init_data=None):
     
     # Parameters
     b.Pmax = pyo.Param(initialize=data['P_max'])
     
     # Variables
-    b.P = pyo.Var(t, initialize={k:0.0 for k in range(1,len(t)+1)}, bounds=(-b.Pmax, b.Pmax), domain=pyo.Reals)
+    b.P = pyo.Var(t, initialize={k:0.0 for k in range(len(t))}, bounds=(-b.Pmax, b.Pmax), domain=pyo.Reals)
     
     # Ports
     b.port_P = Port(initialize={'P': (b.P, Port.Extensive)})
