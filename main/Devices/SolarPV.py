2878d35033aedab1477247e30ad7aa1dbd22bc43
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-17 17:25:09 +0200

    added Pdim

--- main/Devices/SolarPV.py
+++ main/Devices/SolarPV.py
@@ -11,24 +11,23 @@ import pyomo.environ as pyo
 from pyomo.network import *
 
 
-# data: Pinst, forecast(t)
+# data: Pinst, Pmax, forecast(t)
 # init_data: None
 
 def SolarPV(b, t, data, init_data=None):
     
     # Parameters
-    # b._P = pyo.Param(t, initialize=data['P'])
     b.Pinst = pyo.Param(initialize=data['Pinst'])
     b.forecast = pyo.Param(t, initialize=data['forecast'])
     
     # Variables
-    b.P = pyo.Var(t, initialize={k: -data['Pinst']*data['forecast'][k] for k in range(len(t))} , bounds=(-data['Pinst'],0), domain=pyo.Reals)
+    b.P = pyo.Var(t, initialize={k: -data['Pinst']*data['forecast'][k] for k in range(len(t))} , bounds=(-data['Pmax'],0), domain=pyo.Reals)
+    b.Pdim = pyo.Var(initialize=0 , bounds=(0, data['Pmax']-data['Pinst']), domain=pyo.NonNegativeReals)
     
     # Ports
     b.port_P = Port(initialize={'P': (b.P, Port.Extensive)})
     
     # Constraints
     def Constraint_P(_b, _t):
-        # return b._P[_t] == _b.P[_t]
-        return _b.P[_t] == -b.Pinst*b.forecast[_t]
+        return _b.P[_t] == -(b.Pinst+b.Pdim)*b.forecast[_t]
     b.Constraint_P = pyo.Constraint(t, rule=Constraint_P)
