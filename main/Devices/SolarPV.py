6f0b8dd5e67dc2a378015f623398e711faaa3f44
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-17 16:35:44 +0200

    added forecast

--- main/Devices/SolarPV.py
+++ main/Devices/SolarPV.py
@@ -11,22 +11,24 @@ import pyomo.environ as pyo
 from pyomo.network import *
 
 
-# data: P(t)
+# data: Pinst, forecast(t)
 # init_data: None
 
 def SolarPV(b, t, data, init_data=None):
     
     # Parameters
-    b._P = pyo.Param(t, initialize=data['P'])
+    # b._P = pyo.Param(t, initialize=data['P'])
+    b.Pinst = pyo.Param(initialize=data['Pinst'])
+    b.forecast = pyo.Param(t, initialize=data['forecast'])
     
     # Variables
-    b.P = pyo.Var(t, initialize=data['P'], bounds=(None,0), domain=pyo.Reals)
+    b.P = pyo.Var(t, initialize={k: -data['Pinst']*data['forecast'][k] for k in range(len(t))} , bounds=(-data['Pinst'],0), domain=pyo.Reals)
     
     # Ports
     b.port_P = Port(initialize={'P': (b.P, Port.Extensive)})
     
     # Constraints
     def Constraint_P(_b, _t):
-        return b._P[_t] == _b.P[_t]
+        # return b._P[_t] == _b.P[_t]
+        return _b.P[_t] == -b.Pinst*b.forecast[_t]
     b.Constraint_P = pyo.Constraint(t, rule=Constraint_P)
-    
