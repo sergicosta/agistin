06d7b18ea57e9982b1a0e3eadd4996e83c7d0b71
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-19 10:10:01 +0200

    Added Pbuy Psell distinction

--- main/Devices/MainGrid.py
+++ main/Devices/MainGrid.py
@@ -21,8 +21,17 @@ def Grid(b, t, data, init_data=None):
     
     # Variables
     b.P = pyo.Var(t, initialize={k:0.0 for k in range(len(t))}, bounds=(-b.Pmax, b.Pmax), domain=pyo.Reals)
+    b.Psell = pyo.Var(t, initialize={k:0.0 for k in range(len(t))}, bounds=(0, b.Pmax), domain=pyo.NonNegativeReals)
+    b.Pbuy = pyo.Var(t, initialize={k:0.0 for k in range(len(t))}, bounds=(0, b.Pmax), domain=pyo.NonNegativeReals)
     
     # Ports
     b.port_P = Port(initialize={'P': (b.P, Port.Extensive)})
     
     # Constraints
+    def Constraint_P(_b, _t):
+        return _b.P[_t] == _b.Psell[_t] - _b.Pbuy[_t]
+    b.c_P = pyo.Constraint(t, rule=Constraint_P)
+    
+    def Constraint_P0(_b, _t):
+        return 0 == _b.Psell[_t]*_b.Pbuy[_t]
+    b.c_P0 = pyo.Constraint(t, rule=Constraint_P0)
