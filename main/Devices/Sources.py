5820358d3f50b5ce9fcfa2b2eecf2c293d3092d9
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-27 12:01:36 +0200

    Changed Q behaviour to Qin=-Q and Qout=+Q

--- main/Devices/Sources.py
+++ main/Devices/Sources.py
@@ -19,8 +19,8 @@ def Source(b, t, data, init_data=None):
     b.Q = pyo.Param(t, initialize=data['Q'])
     
     # Variables
-    b.Qin = pyo.Var(t, initialize=data['Q'])
-    b.Qout = pyo.Var(t, initialize=data['Q'])
+    b.Qin = pyo.Var(t, initialize=data['Q'], within=pyo.Reals)
+    b.Qout = pyo.Var(t, initialize=data['Q'], within=pyo.Reals)
     
     # Ports
     b.port_Qin = Port(initialize={'Q': (b.Qin, Port.Extensive)})
@@ -28,7 +28,7 @@ def Source(b, t, data, init_data=None):
     
     # Constraints
     def Constraint_Qin(_b, _t):
-        return _b.Qin[_t] == _b.Q[_t]
+        return _b.Qin[_t] == -_b.Q[_t]
     b.c_Qin = pyo.Constraint(t, rule=Constraint_Qin)
     def Constraint_Qout(_b, _t):
         return _b.Qout[_t] == _b.Q[_t]
