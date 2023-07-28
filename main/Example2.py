e1c4af418464d2ca5323ae2cb91fff3ead21fc36
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-07-28 12:43:57 +0200

    Removed S0

--- main/Example2.py
+++ main/Example2.py
@@ -4,6 +4,8 @@
     Optimization usage example of two reservoirs, considering irrigation consumption,
     and two pumps. The pumping station gathers the pumps' power consumption, a solar
     PV plant and a connection point to the public grid.
+    The PV plant can be upgraded to double its size (from 50e3 to 100e3) with a cost of 10 per power unit.
+    The price of selling power to the grid is half of the cost of buying it.
     
     Authors: Sergi Costa Dilmé (CITCEA-UPC), Juan Carlos Olives-Camps (CITCEA-UPC), Paula Muñoz Peña (CITCEA-UPC)
 """
@@ -37,8 +39,6 @@ cost_new_pv = 10
 
 
 # ===== Create the system =====
-
-m.Source0 = pyo.Block()
 m.Reservoir1 = pyo.Block()
 m.Reservoir0 = pyo.Block()
 m.Irrigation1 = pyo.Block()
@@ -49,9 +49,6 @@ m.PV = pyo.Block()
 m.Grid = pyo.Block()
 m.EB = pyo.Block()
 
-
-data_smain = {'Q':[0,0,0,0,0]}
-Source(m.Source0, m.t, data_smain, {})
 
 data_irr = {'Q':[2,1,1,1,1]} # irrigation
 Source(m.Irrigation1, m.t, data_irr, {})
@@ -70,7 +67,7 @@ init_p = {'Q':[0,0,0,0,0], 'H':[20,20,20,20,20], 'n':[1450,1450,1450,1450,1450],
 Pump(m.Pump1, m.t, data_p, init_p)
 Pump(m.Pump2, m.t, data_p, init_p)
 
-data_pv = {'Pinst':50e3, 'Pmax':100e3, 'forecast':[1.0,0.0,0.5,0.2,0.8]} # PV
+data_pv = {'Pinst':50e3, 'Pmax':100e3, 'forecast':[0.0,0.2,0.8,1.0,0.1]} # PV
 SolarPV(m.PV, m.t, data_pv)
 
 Grid(m.Grid, m.t, {'Pmax':100e3}) # grid
@@ -80,15 +77,14 @@ EB(m.EB, m.t)
 
 
 # Connections
-m.p1r0 = Arc(ports=(m.Pump1.port_Qin, m.Reservoir0.port_Qout), directed=True)
+m.p1r0 = Arc(ports=(m.Pump1.port_Qin, m.Reservoir0.port_Q), directed=True)
 m.p1c1_Q = Arc(ports=(m.Pump1.port_Qout, m.Pipe1.port_Q), directed=True)
 m.p1c1_H = Arc(ports=(m.Pump1.port_H, m.Pipe1.port_H), directed=True)
-m.p2r0 = Arc(ports=(m.Pump2.port_Qin, m.Reservoir0.port_Qout), directed=True)
+m.p2r0 = Arc(ports=(m.Pump2.port_Qin, m.Reservoir0.port_Q), directed=True)
 m.p2c1_Q = Arc(ports=(m.Pump2.port_Qout, m.Pipe1.port_Q), directed=True)
 m.p2c1_H = Arc(ports=(m.Pump2.port_H, m.Pipe1.port_H), directed=True)
-m.c1r1 = Arc(ports=(m.Pipe1.port_Q, m.Reservoir1.port_Qin), directed=True)
-m.r0s0 = Arc(ports=(m.Source0.port_Qout, m.Reservoir0.port_Qin), directed=True)
-m.r1i1 = Arc(ports=(m.Irrigation1.port_Qin, m.Reservoir1.port_Qout), directed=True)
+m.c1r1 = Arc(ports=(m.Pipe1.port_Q, m.Reservoir1.port_Q), directed=True)
+m.r1i1 = Arc(ports=(m.Irrigation1.port_Qin, m.Reservoir1.port_Q), directed=True)
 m.ebp1 = Arc(ports=(m.Pump1.port_P, m.EB.port_P), directed=True)
 m.ebp2 = Arc(ports=(m.Pump2.port_P, m.EB.port_P), directed=True)
 m.grideb = Arc(ports=(m.Grid.port_P, m.EB.port_P), directed=True)
@@ -101,7 +97,7 @@ pyo.TransformationFactory("network.expand_arcs").apply_to(m) # apply arcs to mod
 
 # Objective function
 def obj_fun(m):
-	return sum((m.Grid.Pbuy[t]-m.Grid.Psell[t])*m.cost[t] for t in l_t ) + m.PV.Pdim*cost_new_pv
+	return sum((m.Grid.Pbuy[t]*m.cost[t] - m.Grid.Psell[t]*m.cost[t]/2) for t in l_t ) + m.PV.Pdim*cost_new_pv
 m.goal = pyo.Objective(rule=obj_fun, sense=pyo.minimize)
 
 instance = m.create_instance()
@@ -116,25 +112,25 @@ instance.PV.Pdim.pprint()
 # RESULTS
     # W : Size=5, Index=t
     #     Key : Lower : Value              : Upper : Fixed : Stale : Domain
-    #       0 :     0 :   2.99999999999992 :    20 : False : False : NonNegativeReals
-    #       1 :     0 :  2.156369061912749 :    20 : False : False : NonNegativeReals
-    #       2 :     0 : 1.8436309280858596 :    20 : False : False : NonNegativeReals
-    #       3 :     0 : 0.9999999900000824 :    20 : False : False : NonNegativeReals
+    #       0 :     0 : 3.0000000000000004 :    20 : False : False : NonNegativeReals
+    #       1 :     0 :  2.051624535242702 :    20 : False : False : NonNegativeReals
+    #       2 :     0 : 1.7161061718196515 :    20 : False : False : NonNegativeReals
+    #       3 :     0 : 0.9741875933941936 :    20 : False : False : NonNegativeReals
     #       4 :     0 :                0.0 :    20 : False : False : NonNegativeReals
     # W : Size=5, Index=t
     #     Key : Lower : Value             : Upper : Fixed : Stale : Domain
-    #       0 :     0 :  5.00000000000008 :    20 : False : False : NonNegativeReals
-    #       1 :     0 : 4.843630938087251 :    20 : False : False : NonNegativeReals
-    #       2 :     0 :  4.15636907191414 :    20 : False : False : NonNegativeReals
-    #       3 :     0 : 4.000000009999917 :    20 : False : False : NonNegativeReals
+    #       0 :     0 : 4.999999999999999 :    20 : False : False : NonNegativeReals
+    #       1 :     0 : 4.948375464757298 :    20 : False : False : NonNegativeReals
+    #       2 :     0 : 4.283893828180348 :    20 : False : False : NonNegativeReals
+    #       3 :     0 : 4.025812406605806 :    20 : False : False : NonNegativeReals
     #       4 :     0 : 4.000000009999997 :    20 : False : False : NonNegativeReals
     # P : Size=5, Index=t
-    #     Key : Lower     : Value              : Upper    : Fixed : Stale : Domain
-    #       0 : -100000.0 :           100000.0 : 100000.0 : False : False :  Reals
-    #       1 : -100000.0 : -34090.53926322234 : 100000.0 : False : False :  Reals
-    #       2 : -100000.0 :          -100000.0 : 100000.0 : False : False :  Reals
-    #       3 : -100000.0 : -14090.53916352628 : 100000.0 : False : False :  Reals
-    #       4 : -100000.0 :  80000.00040001729 : 100000.0 : False : False :  Reals
+    #     Key : Lower     : Value                  : Upper    : Fixed : Stale : Domain
+    #       0 : -100000.0 :  9.444173782089441e-09 : 100000.0 : False : False :  Reals
+    #       1 : -100000.0 : -8.895566856661031e-09 : 100000.0 : False : False :  Reals
+    #       2 : -100000.0 :              -100000.0 : 100000.0 : False : False :  Reals
+    #       3 : -100000.0 : -8.894453079458348e-09 : 100000.0 : False : False :  Reals
+    #       4 : -100000.0 :  9.520949369890758e-09 : 100000.0 : False : False :  Reals
     # Pdim : Size=1, Index=None
-    #     Key  : Lower : Value   : Upper   : Fixed : Stale : Domain
-    #     None :     0 : 50000.0 : 50000.0 : False : False : NonNegativeReals
+    #     Key  : Lower : Value             : Upper   : Fixed : Stale : Domain
+    #     None :     0 : 6271.117831362812 : 50000.0 : False : False : NonNegativeReals
