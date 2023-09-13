f41cb328c34e6caa959e3ef561785550d75be3fe
colives <carlos.olives.camps@gmail.com>
2023-09-13 13:27:01 +0200

    Beta version of DataParser

--- main/DataParser.py
+++ main/DataParser.py
@@ -3,17 +3,25 @@ AGISTIN project
 
 .\DataParser.py
 
-DataParser generates a .json file that feeds the builder function.
+DataParser generates a .json file form excel files. The .json generated feeds 
+the builder function.
 """
 
 import pandas as pd
 
-NameTest = 'Example3'
+def write_list(f, df, df_time, k, val):
+    for it in df_time[k]:
+        aux = it.split('_')
+        if aux[0] == df[k]["Name"][val]:
+            f.write(f'"{aux[1]}":{list(df_time[k][it])},')
+
+NameTest = 'Test1'
 T = 5
 dt = 1
 
 df = pd.read_excel(f'Cases/{NameTest}.xlsx', sheet_name=None)
 df_time = pd.read_excel(f'Cases/{NameTest}_time.xlsx', sheet_name=None)
+special = ['SolarPV','Source']
 
 with open(f'Cases/{NameTest}.json', 'w') as f:
     f.write('{\n')
@@ -28,8 +36,16 @@ with open(f'Cases/{NameTest}.json', 'w') as f:
                     f.write(f'"{it}":{df[k][it][val]},')
             if k == 'Reservoir':
                 f.write(f'"dt":{dt}')
+            if k in special:
+                #  Time values as data
+                write_list(f, df, df_time, k, val)
             f.write('},\n')
-            f.write('\t "init_data":{},\n')
+            #  Time values as Initial data
+            f.write('\t "init_data":{')
+            if k not in special:
+                write_list(f, df, df_time, k, val)
+            f.write('},\n')
+            #  CONNECTIONS
             f.write('\t "conns":{')
             try:
                 con = df[k]['CONNECTION'][val]
