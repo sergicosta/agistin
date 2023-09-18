3ca5b17ca0f66844f0027e274164856b39cff8e9
Sergi Costa <sergi.costa.dilme@upc.edu>
2023-09-18 15:14:22 +0200

    Added battery

---
+++ main/Devices/Batteries.py
@@ -0,0 +1,102 @@
+# AGISTIN project 
+# .\Devices\Batteries.py
+"""
+Battery pyomo block containing the characteristics of a battery.
+"""
+
+import pyomo.environ as pyo
+from pyomo.network import *
+
+
+# data: W0, Wmin, Wmax
+# init_data: Q(t), W(t)
+    
+def Battery(b, t, data, init_data):
+    
+    """
+    Simple Battery.
+    
+    Modifies its energy state :math:`E(t)` from an initial state :math:`E_0` according to 
+    the charge or discharge power :math:`P(t)>0` is :math:`P_{ch}(t)` and :math:`P(t)<0` is :math:`P_{disc}(t)`.
+    
+    The state of charge `SOC(t)` is computed and taken into account as well.
+    
+    :param b: pyomo ``Block()`` to be set
+    :param t: pyomo ``Set()`` referring to time
+    :param data: data ``dict``
+    :param init_data: init_data ``dict``
+    
+    data
+         - 'dt': time delta :math:`\Delta t`
+         - 'E0': Initial energy :math:`E_0`
+         - 'Emax': Maximum battery energy :math:`E_{max}`
+         - 'SOCmin': Minimum allowed SOC :math:`SOC_{min}`
+         - 'SOCmax': Maximum allowed SOC :math:`SOC_{max}`
+         
+    init_data
+         - 'E': Energy :math:`E(t)` as a ``list``
+         - 'P': Power :math:`P(t)` as a ``list``
+    
+    Pyomo declarations    
+        - Parameters: 
+            - dt
+            - E0
+            - Emax
+            - SOCmin
+            - SOCmax
+            - Pmax
+        - Variables: 
+            - E (t) bounded :math:`E(t) \in [E_{max}\cdot SOC_{min}, E_{max}\cdot SOC_{max}]`
+            - P (t) bounded :math:`P(t) \in [-P_{max}, P_{max}]`
+            - Pch (t) bounded :math:`P_{ch}(t) \in [0, P_{max}]`
+            - Pdisc (t) bounded :math:`P_{disc}(t) \in [0, P_{max}]`
+            - SOC (t) bounded :math:`SOC(t) \in [SOC_{min}, SOC_{max}]`
+        - Ports: 
+            - port_P @ P (Extensive)
+        - Constraints:
+            - c_P: :math:`P(t) = P_{ch}(t) - P_{disc}(t)`
+            - c_P0: :math:`0 = P_{ch}(t) \cdot P_{disc}(t)`
+            - c_SOC: :math:`SOC(t) = E(t) \cdot E_{max}`
+            - c_E: 
+                - :math:`E(t) = E(t-1) + \Delta t \cdot P(t) \quad` if  :math:`t>0`
+                - :math:`W(t) = E_0 + \Delta t \cdot P(t) \quad` otherwise
+    """
+    
+    b.dt = data['dt']
+    
+    # Parameters
+    b.E0 = pyo.Param(initialize=data['E0'])
+    b.Emax = pyo.Param(initialize=data['Emax'])
+    b.SOCmax = pyo.Param(initialize=data['SOCmax'])
+    b.SOCmin = pyo.Param(initialize=data['SOCmin'])
+    b.Pmax = pyo.Param(initialize=data['Pmax'])
+    
+    # Variables
+    b.E  = pyo.Var(t, initialize=init_data['E'], bounds=(data['Emax']*data['SOCmin'], data['Emax']*data['SOCmax']), within=pyo.NonNegativeReals)
+    b.P = pyo.Var(t, initialize=init_data['P'], bounds=(-data['Pmax'], data['Pmax']), within=pyo.Reals)
+    b.Pch = pyo.Var(t, initialize={k:0.0 for k in range(len(t))}, bounds=(0, data['Pmax']), within=pyo.NonNegativeReals)
+    b.Pdisc = pyo.Var(t, initialize={k:0.0 for k in range(len(t))}, bounds=(0, data['Pmax']), within=pyo.NonNegativeReals)
+    b.SOC = pyo.Var(t, initialize=init_data['E']/data['Emax'], bounds=(data['SOCmin'], data['SOCmax']), within=pyo.NonNegativeReals)
+
+    # Ports
+    b.port_P = Port(initialize={'P': (b.P, Port.Extensive)})
+
+    # Constraints
+    def Constraint_P(_b, _t):
+        return _b.P[_t] == _b.Pch[_t] - _b.Pdisc[_t]
+    b.c_P = pyo.Constraint(t, rule = Constraint_P)
+    
+    def Constraint_P0(_b, _t):
+        return 0 == _b.Pch[_t] * _b.Pdisc[_t]
+    b.c_P0 = pyo.Constraint(t, rule = Constraint_P0)
+    
+    def Constraint_SOC(_b, _t):
+        return _b.SOC[_t] == _b.E[_t] / _b.Emax
+    b.c_SOC = pyo.Constraint(t, rule = Constraint_SOC)
+    
+    def Constraint_E(_b, _t):
+        if _t>0:
+            return _b.E[_t] == _b.E[_t-1] + b.dt * (_b.Pch[_t] - _b.Pdisc[_t])
+        else:
+            return b.E[_t] == _b.E0 + b.dt * (_b.Pch[_t] - _b.Pdisc[_t])
+    b.c_E = pyo.Constraint(t, rule = Constraint_E)
