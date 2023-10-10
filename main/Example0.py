d592936f9155919fe989f0e1e6ebe6d638a33749
Daniel <145111510+DPombo-EPRI@users.noreply.github.com>
2023-10-10 12:34:59 +0200

    Cleaning update
    Minor update including a new function file for utilities. For now it only includes a function that copycats "clear all, clc" in Matlab, but for Spyder. I am using it as a way to test whether I can push in the repo more than anything.

--- main/Example0.py
+++ main/Example0.py
@@ -14,6 +14,11 @@ from pyomo.network import *
 from Devices.Reservoirs import Reservoir_Ex0
 from Devices.Sources import Source
 
+# Import useful functions
+from Utilities import clear_clc
+
+#clean console and variable pane
+clear_clc() #consider removing if you are not working with Spyder
 
 # model
 m = pyo.ConcreteModel()
---
+++ main/Utilities.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 11:37:28 2023
+
+@author: Daniel V Pombo
+
+.\ Utilities.py
+
+Generic functions and utilities used recurively.
+"""
+
+def clear_clc(): 
+    """
+    Applies clear and clc similar to Matlab in Spyder
+    """
+    try:
+        from IPython import get_ipython
+        get_ipython().magic('clear')
+        get_ipython().magic('reset -f')
+    except:
+        pass 
