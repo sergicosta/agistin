bcd53eacc87879bcf35646b2a1ad4c24fb62a0b4
Sergi Costa <sergi.costa.dilme@upc.edu>
2024-05-02 11:41:53 +0200

    OSMSES final

--- main/OSMSES.py
+++ main/OSMSES.py
@@ -19,11 +19,12 @@ import seaborn as sns
 from Devices.Reservoirs import Reservoir
 from Devices.Sources import Source
 from Devices.Pipes import Pipe
-from Devices.Pumps import RealPump
+from Devices.Pumps import RealPump, ReversibleRealPump
 from Devices.EB import EB
 from Devices.SolarPV import SolarPV
 from Devices.MainGrid import Grid
 from Devices.Batteries import Battery
+from Devices.Turbines import Turbine
 
 
 # model
@@ -41,6 +42,7 @@ m.cost = pyo.Param(m.t, initialize=l_cost)
 cost_pv_P = 0.00126
 cost_bat_P = 0.00171
 cost_bat_E = 0.00856
+cost_turb_P = 0.00143
 
 # ===== Create the system =====
 m.Reservoir1 = pyo.Block()
@@ -48,6 +50,7 @@ m.Reservoir0 = pyo.Block()
 m.Irrigation1 = pyo.Block()
 m.Pump1 = pyo.Block()
 m.Pump2 = pyo.Block()
+m.Turb1 = pyo.Block()
 m.Pipe1 = pyo.Block()
 m.PV = pyo.Block()
 m.Grid = pyo.Block()
@@ -59,7 +62,7 @@ data_irr = {'Q':[0.1,0.8,0.8,0.8,0.1]} # irrigation
 Source(m.Irrigation1, m.t, data_irr, {})
 
 data_res0 = {'dt':1, 'W0':5, 'Wmin':0, 'Wmax':10, 'zmin':0, 'zmax':0.05}
-data_res1 = {'dt':1, 'W0':1, 'Wmin':0, 'Wmax':1, 'zmin':0.75, 'zmax':0.80}
+data_res1 = {'dt':1, 'W0':1, 'Wmin':0, 'Wmax':10, 'zmin':0.75, 'zmax':0.80}
 init_res = {'Q':[0]*T, 'W':[0.5]*T}
 
 Reservoir(m.Reservoir1, m.t, data_res1, init_res)
@@ -69,10 +72,14 @@ data_c1 = {'K':0.01, 'Qmax':5} # canal
 init_c1 = {'Q':[0]*T, 'H':[1]*T, 'H0':[1]*T, 'zlow':[0.01]*T, 'zhigh':[0.75]*T}
 Pipe(m.Pipe1, m.t, data_c1, init_c1)
 
-data_p = {'A':1, 'B':0.05, 'n_n':1, 'eff':0.9, 'Qmax':2, 'Qnom':1, 'Pmax':9810*1*1, 'Qmin':0.5, 'Qmax':1.1} # pumps (both equal)
+data_p = {'A':1, 'B':0.05, 'n_n':1, 'eff':0.9, 'Qmax':2, 'Qnom':1, 'Pmax':9810*1*1, 'Qmin':0.5, 'Qmax':1.1, 'eff_t':0.5, 'S':1/4.5} # pumps (both equal)
 init_p = {'Q':[0]*T, 'H':[1]*T, 'n':[1]*T, 'Pe':[9810*1*1]*T}
 RealPump(m.Pump1, m.t, data_p, init_p)
 RealPump(m.Pump2, m.t, data_p, init_p)
+
+data_t = {'eff':0.85, 'Pmax':1.5*9810}
+init_t = {'Q':[0]*T, 'H':[1]*T, 'Pe':[-9810]*T}
+Turbine(m.Turb1, m.t, data_t, init_t)
 
 data_pv = {'Pinst':9810, 'Pmax':9810*2, 'forecast':[0,0.1,0.6,1.2,0.2], 'eff': 0.98} # PV
 SolarPV(m.PV, m.t, data_pv)
@@ -82,7 +89,7 @@ Grid(m.Grid, m.t, {'Pmax':9810e3}) # grid
 EB(m.EB, m.t)
 
 #Battery data
-data = {'E0':500,'SOCmax':1,
+data = {'E0':1000,'SOCmax':1,
         'SOCmin':0.2,'Pmax':9810*2,
         'Einst':1962,'Pinst':4905,
         'Emax':9810*2,'rend_ch':0.9,
@@ -120,6 +127,11 @@ m.p2c1_Q = Arc(ports=(m.Pump2.port_Qout, m.Pipe1.port_Q), directed=True)
 m.p2c1_H = Arc(ports=(m.Pump2.port_H, m.Pipe1.port_H), directed=True)
 m.ebp2 = Arc(ports=(m.Pump2.port_P, m.EB.port_P), directed=True)
 
+m.t1r0 = Arc(ports=(m.Turb1.port_Qout, m.Reservoir0.port_Q), directed=True)
+m.t1c1_Q = Arc(ports=(m.Turb1.port_Qin, m.Pipe1.port_Q), directed=True)
+m.t1c1_H = Arc(ports=(m.Turb1.port_H, m.Pipe1.port_H), directed=True)
+m.t1eb = Arc(ports=(m.Turb1.port_P, m.EB.port_P), directed=True)
+
 m.c1r1 = Arc(ports=(m.Pipe1.port_Q, m.Reservoir1.port_Q), directed=True)
 m.c1r1_z = Arc(ports=(m.Reservoir1.port_z, m.Pipe1.port_zhigh), directed=True)
 m.c1r0_z = Arc(ports=(m.Reservoir0.port_z, m.Pipe1.port_zlow), directed=True)
@@ -140,7 +152,7 @@ import os
 
 # Objective function
 def obj_fun(m):
-	return sum((m.Grid.Pbuy[t]*m.cost[t] - m.Grid.Psell[t]*m.cost[t]/2) for t in l_t ) + m.PV.Pdim*cost_pv_P + m.Battery.Edim*cost_bat_E + m.Battery.Pdim*cost_bat_P
+	return sum((m.Grid.Pbuy[t]*m.cost[t] - m.Grid.Psell[t]*m.cost[t]/2) for t in l_t ) + m.PV.Pdim*cost_pv_P + m.Battery.Edim*cost_bat_E + m.Battery.Pdim*cost_bat_P + m.Turb1.Pdim*cost_turb_P
 m.goal = pyo.Objective(rule=obj_fun, sense=pyo.minimize)
 
 instance = m.create_instance()
@@ -156,7 +168,7 @@ results.write()
 
 # with open("couenne.opt", "w") as file:
 #     file.write('''time_limit 100000
-#                 convexification_cuts 3
+#                 convexification_cuts 2
 #                 convexification_points 2
 #                 delete_redundant yes
 #                 use_quadratic no
@@ -211,6 +223,8 @@ for i in range(len(instance._decl_order)):
 
 file = './results/OSMSES/OSMSES'
 df_out.to_csv(file+'.csv')
+df_param.to_csv(file+'_param.csv')
+df_size.to_csv(file+'_size.csv')
 results.write(filename=file+'_results.txt')
 with open(file+'_results.txt','a') as f:
     f.write('\nGOAL VALUE:\n'+str(value(instance.goal))+'\n')
@@ -224,7 +238,12 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import numpy as np
 
-plt.rcParams['font.size']=9
+file = './results/OSMSES/OSMSES_turb'
+df_out = pd.read_csv(file+'.csv').drop('Unnamed: 0',axis=1)
+df_param = pd.read_csv(file+'_param.csv').drop('Unnamed: 0',axis=1)
+df_size = pd.read_csv(file+'_size.csv').drop('Unnamed: 0',axis=1)
+
+plt.rcParams['font.size']=8
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = 'Lucida Bright'
 plt.rc('axes', unicode_minus=False)
@@ -234,8 +253,7 @@ df = pd.read_csv(file+'.csv')
 df['PV.Pf'] = -df['PV.forecast']*(df_param['PV.Pinst'].iloc[0] + df_size['PV.Pdim'])
 
 # Hydro
-fig = plt.figure(figsize=(3.4, 2.5))
-plt.rc('font',size=9)
+fig = plt.figure(figsize=(3.4, 2.3))
 fig.add_subplot(2,1,1)
 ax1 = sns.lineplot(data=df, x='t',y='Reservoir1.W', label='R1', color='grey', marker='^')
 sns.lineplot(data=df, x='t',y='Reservoir0.W', label='R0', color='k', marker='v')
@@ -245,16 +263,16 @@ plt.xlabel('Time')
 plt.ylabel('Volume')
 plt.xticks(range(T),range(T))
 ax2 = fig.add_subplot(2,1,2)
-df.plot(y=['Pump1.Qout','Pump2.Qout','Irrigation1.Qout'], kind='bar', stacked=False, ylabel='Q', ax=ax2, 
-        color=['lightgrey','lightgrey','k'], edgecolor='dimgray', label=['P1','P2','Irr'], sharex=True)
+df.plot(y=['Pump1.Qout','Pump2.Qout','Irrigation1.Qout', 'Turb1.Qout'], kind='bar', stacked=False, ylabel='Q', ax=ax2, 
+        color=['lightgrey','lightgrey','k', 'lightgrey'], edgecolor='dimgray', label=['P1','P2','Irr', 'T1'], sharex=True)
 bars = ax2.patches
-patterns =('////', '\\\\\\\\',None)
+patterns =('////', '\\\\\\\\',None, 'xxxxx')
 hatches = [p for p in patterns for i in range(len(df))]
 for bar, hatch in zip(bars, hatches):
     bar.set_hatch(hatch)
 ax2.legend()
-plt.axhline(y=df_param['Pump1.Qmin'].iloc[0], color='dimgray', linestyle='dashed')
-plt.axhline(y=df_param['Pump1.Qmax'].iloc[0], color='dimgray', linestyle='dashed')
+# plt.axhline(y=df_param['Pump1.Qmin'].iloc[0], color='dimgray', linestyle='dashed')
+# plt.axhline(y=df_param['Pump1.Qmax'].iloc[0], color='dimgray', linestyle='dashed')
 plt.xlabel('Time')
 plt.ylabel('Flow')
 plt.xticks(range(T),range(T), rotation=0)
@@ -264,19 +282,18 @@ plt.rcParams['savefig.format']='pdf'
 plt.savefig(file + '_VQ', dpi=300)
 
 # Power
-fig = plt.figure(figsize=(3.4, 2.0))
-plt.rc('font',size=9)
+fig = plt.figure(figsize=(3.4, 2.2))
 # plt.suptitle('Power consumption')
 ax1 = fig.add_subplot(1,1,1)
-df.apply(lambda x: x/9810).plot(y=['Pump1.Pe','Pump2.Pe','PV.P', 'Grid.P'], kind='bar', stacked=True, ax=ax1, ylabel='P',
-                                color=['lightgrey','lightgrey','dimgrey','k'], edgecolor='dimgray')
+df.apply(lambda x: x/9810).plot(y=['Pump1.Pe','Pump2.Pe','PV.P', 'Grid.P', 'Turb1.Pe', 'Battery.P'], kind='bar', stacked=True, ax=ax1, ylabel='P',
+                                color=['lightgrey','lightgrey','dimgrey','k', 'lightgrey', 'lightgrey'], edgecolor='dimgray')
 bars = ax1.patches
-patterns =('////', '\\\\\\\\',None,None)
+patterns =('////', '\\\\\\\\',None,None,'xxxxx',None)
 hatches = [p for p in patterns for i in range(len(df))]
 for bar, hatch in zip(bars, hatches):
     bar.set_hatch(hatch)
-ax1.legend(['$P_{p1}$','$P_{p2}$','$P_{PV}$', '$P_{g}$'],loc='upper center', bbox_to_anchor=(0.5, -0.2),
-          fancybox=False, shadow=False, ncol=5)
+ax1.legend(['$P_{p1}$','$P_{p2}$','$P_{PV}$', '$P_{g}$', '$P_{t1}$', '$P_{bat}$'],loc='upper center', bbox_to_anchor=(0.525, -0.2),
+          fancybox=False, shadow=False, ncol=4)
 ax1.axhline(0,color='k')
 ax1.set_xticks(range(T), labels=range(T), rotation=0)
 ax2 = plt.twinx()
@@ -285,15 +302,15 @@ ax2.set_xticks(range(T), labels=range(T), rotation=0)
 plt.ylabel('Price')
 plt.xlabel('Time')
 # plt.title('Power consumption')
-plt.show()
 plt.tight_layout()
+plt.show()
 plt.rcParams['savefig.format']='pdf'
 plt.savefig(file + '_P', dpi=300)
 
 # Battery
-fig = plt.figure(figsize=(3.4, 2))
+fig = plt.figure(figsize=(3.4, 1.6))
 sns.lineplot(data=df, x='t',y=df['Battery.E']/9810, label='Energy', color='k', marker='.')
-sns.barplot(data=df, x='t', y=df['Battery.P']/9810, label='Power', color='darkgrey')
+sns.barplot(data=df, x='t', y=df['Battery.P']/9810, label='Power', color='lightgrey', edgecolor='dimgray')
 plt.axhline(0,color='k')
 plt.legend(loc='lower left')
 plt.xlabel('Time')
@@ -309,7 +326,7 @@ df_plot2 = df_param[['PV.Pinst', 'Battery.Pinst', 'Battery.Einst']]
 df_plot2.columns = ['PV.Pdim', 'Battery.Pdim', 'Battery.Edim']
 df_plot = pd.concat([df_plot2, df_size])
 
-ax = df_param[['PV.Pmax','Battery.Pmax','Battery.Emax']].apply(lambda x: x/9810).transpose().plot(kind='bar',stacked=False, 
+ax = df_param[['PV.Pmax','Battery.Pmax','Battery.Emax','Turb1.Pmax']].apply(lambda x: x/9810).transpose().plot(kind='bar',stacked=False, 
     rot=0, color='lightgrey', alpha=0.5, figsize=(3.4, 1.5), hatch="////", edgecolor='k')
 df_plot.apply(lambda x: x/9810).transpose().plot(kind='bar',stacked=True, rot=0, ax=ax, color=['k','gray'])
 ax.spines['right'].set_visible(False)
@@ -318,7 +335,7 @@ ax.spines['top'].set_visible(False)
 plt.legend(['Max.', 'Inst.', 'Dim.'],loc='upper center', bbox_to_anchor=(0.5, -0.3),
           fancybox=False, shadow=False, ncol=5)
 plt.ylabel('Sizing')
-plt.xticks(range(3), labels=['PV $P$','Bat. $P$','Bat. $E$'], rotation=0)
+plt.xticks(range(4), labels=['PV $P$','Bat. $P$','Bat. $E$', 'Turb. $P$'], rotation=0)
 plt.yticks(range(3), labels=range(3), rotation=0)
 plt.tight_layout()
 plt.show()
