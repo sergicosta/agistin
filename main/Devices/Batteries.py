fb51aee2a3637e5d0e8f3954b6089f52821a1ed2
nishat jillani <142987959+NishatJillani@users.noreply.github.com>
2024-07-09 00:22:18 +0200

    Add files via upload

--- main/Devices/Batteries.py
+++ main/Devices/Batteries.py
@@ -262,63 +262,63 @@ def Battery_Ex0(b, t, data, init_data):
 
 def Battery_MV(b, t, data, init_data):
     
-    """
-    This battery model in intended to be used for he optmization examples using frequency data, for example
-    for the provision of frecuency containtment reserve. The model was implemented by Manuel Valois he 19.03.2024
+    # """
+    # This battery model in intended to be used for he optmization examples using frequency data, for example
+    # for the provision of frecuency containtment reserve. The model was implemented by Manuel Valois he 19.03.2024
     
-    The current version is missing he deliitaion of a maximun and minimium SOC 
+    # The current version is missing he deliitaion of a maximun and minimium SOC 
     
-    :param b: pyomo ``Block()`` to be set
-    :param t: pyomo ``Set()`` referring to time
-    :param data: data ``dict``
-    :param init_data: init_data ``dict``
+    # :param b: pyomo ``Block()`` to be set
+    # :param t: pyomo ``Set()`` referring to time
+    # :param data: data ``dict``
+    # :param init_data: init_data ``dict``
         
-    data:
-         - 'E0': Initial energy :math:`E_0`
-         - 'Emax': Maximum battery energy :math:`E_{max}`
-         - 'SOCmin': Minimum allowed SOC :math:`SOC_{min}` in p.u.
-         - 'SOCmax': Maximum allowed SOC :math:`SOC_{max}` in p.u.
-         - 'Pmax': Maximum delivered/absorbed power :math:`P_{max}`
-         - 'Pinst': Power already installed :math:`P_{inst}`
-         - 'Einst': Energy storage already installed :math:`E_{inst}`
+    # data:
+    #      - 'E0': Initial energy :math:`E_0`
+    #      - 'Emax': Maximum battery energy :math:`E_{max}`
+    #      - 'SOCmin': Minimum allowed SOC :math:`SOC_{min}` in p.u.
+    #      - 'SOCmax': Maximum allowed SOC :math:`SOC_{max}` in p.u.
+    #      - 'Pmax': Maximum delivered/absorbed power :math:`P_{max}`
+    #      - 'Pinst': Power already installed :math:`P_{inst}`
+    #      - 'Einst': Energy storage already installed :math:`E_{inst}`
              
-    init_data:
-         - 'E': Energy :math:`E(t)` as a ``list``
-         - 'P': Power :math:`P(t)` as a ``list``
+    # init_data:
+    #      - 'E': Energy :math:`E(t)` as a ``list``
+    #      - 'P': Power :math:`P(t)` as a ``list``
          
-    Pyomo declaration
-        - Parameters: 
-            - dt
-            - E0
-            - Emax
-            - Pmax
-            - Pdim
-            - Edim
-            - eff_ch
-            - eff_disc
-        - Variables: 
-            - E (t) bounded :math:`E(t) \in [E_{max}\cdot SOC_{min}, E_{max}\cdot SOC_{max}]`
-            - P (t) bounded :math:`P(t) \in [-P_{max}, P_{max}]`
-            - Pch (t) bounded :math:`P_{ch}(t) \in [0, P_{max}]`
-            - Pdisc (t) bounded :math:`P_{disc}(t) \in [0, P_{max}]`
-            - SOC (t) bounded :math:`SOC(t) \in [SOC_{min}, SOC_{max}]`
-            - Edim bounded :math:`E_{dim} \in [0, E_ {max} - E_{inst}]`
-            - Pdim bounded :math:`P_{dim} \in [0, P_ {max} - P_{inst}]`
-        - Ports: 
-            - port_P @ P (Extensive)
-        - Constraints:
-            - c_P: :math:`P(t) = P_{ch}(t) - P_{disc}(t)`
-            - c_P0: :math:`0 = P_{ch}(t) \cdot P_{disc}(t)`
-            - c_SOC: :math:`SOC(t) = E(t) /(E_{dim}+E_{inst}`
-            - c_ch: :math:`Pch(t) \leq (P{inst} + P{dim})`
-            - c_disc: :math:`Pdisc(t) \leq (P{inst} + P{dim})`
-            - c_Emax: :math:`E(t) \leq (E{inst} + E{dim})\cdot SOC{max}`
-            - c_Emin: :math:`E(t) \leq (E{inst} + E{dim})\cdot SOC{min}`
-            - c_E: 
-                - :math:`E(t) = E(t-1) + \Delta t \cdot P(t) \quad` if  :math:`t>0`
-                - :math:`E(t) = E_0 + \Delta t \cdot P(t) \quad` otherwise
+    # Pyomo declaration
+    #     - Parameters: 
+    #         - dt
+    #         - E0
+    #         - Emax
+    #         - Pmax
+    #         - Pdim
+    #         - Edim
+    #         - eff_ch
+    #         - eff_disc
+    #     - Variables: 
+    #         - E (t) bounded :math:`E(t) \in [E_{max}\cdot SOC_{min}, E_{max}\cdot SOC_{max}]`
+    #         - P (t) bounded :math:`P(t) \in [-P_{max}, P_{max}]`
+    #         - Pch (t) bounded :math:`P_{ch}(t) \in [0, P_{max}]`
+    #         - Pdisc (t) bounded :math:`P_{disc}(t) \in [0, P_{max}]`
+    #         - SOC (t) bounded :math:`SOC(t) \in [SOC_{min}, SOC_{max}]`
+    #         - Edim bounded :math:`E_{dim} \in [0, E_ {max} - E_{inst}]`
+    #         - Pdim bounded :math:`P_{dim} \in [0, P_ {max} - P_{inst}]`
+    #     - Ports: 
+    #         - port_P @ P (Extensive)
+    #     - Constraints:
+    #         - c_P: :math:`P(t) = P_{ch}(t) - P_{disc}(t)`
+    #         - c_P0: :math:`0 = P_{ch}(t) \cdot P_{disc}(t)`
+    #         - c_SOC: :math:`SOC(t) = E(t) /(E_{dim}+E_{inst}`
+    #         - c_ch: :math:`Pch(t) \leq (P{inst} + P{dim})`
+    #         - c_disc: :math:`Pdisc(t) \leq (P{inst} + P{dim})`
+    #         - c_Emax: :math:`E(t) \leq (E{inst} + E{dim})\cdot SOC{max}`
+    #         - c_Emin: :math:`E(t) \leq (E{inst} + E{dim})\cdot SOC{min}`
+    #         - c_E: 
+    #             - :math:`E(t) = E(t-1) + \Delta t \cdot P(t) \quad` if  :math:`t>0`
+    #             - :math:`E(t) = E_0 + \Delta t \cdot P(t) \quad` otherwise
 
-     """     
+    #  """     
     
     # Parameters
     b.E0 = pyo.Param(initialize=data['E0'])       # Energy storage at the battery at the beggiing of the simulation (_t=0)
@@ -352,6 +352,17 @@ def Battery_MV(b, t, data, init_data):
     #b.SOC = pyo.Var(t,   within=pyo.NonNegativeReals) # bounds = (0.0,data['Einst']) , within=pyo.NonNegativeReals
     b.Pout = pyo.Var(t, initialize= 0.0,  within=pyo.Reals)
     b.EENS = pyo.Var(t, within=pyo.NonNegativeReals)
+    
+    
+    
+    
+    # New variables NISHAT
+    
+    # Define new variables Y(t) and X(t) with initialization
+    b.Y = pyo.Var(t, initialize=0.0, within=pyo.Reals)
+    b.X = pyo.Var(t, initialize=0.0, within=pyo.Reals)
+
+
 
     b.POutcharged = pyo.Var (t,  within=pyo.NonNegativeReals)
     #b.POutdischarged = pyo.Var(t, initialize= b.Pout,  within=pyo.NonNegativeReals)
@@ -390,6 +401,20 @@ def Battery_MV(b, t, data, init_data):
     def Constraint_FeedInOut(b, t):                                                       #This constraint calculates the amount of power to be delivered
             return (b.Pfeedin[t]*b.Pfeedout[t] == 0.0)
     b.Constraint_FeedInOut = pyo.Constraint(t, rule=Constraint_FeedInOut)
+    
+    
+    
+    # new Constraint NISHAT
+    
+    # Constraint to set the values of Y(t)
+    def Y_constraint(b, t):
+        return b.Y[t] == b.Pfeedin[t] + b.PowerFCRDisCharge[t]
+    b.YConstraint = pyo.Constraint(t, rule=Y_constraint)
+
+    # Constraint to set the values of X(t)
+    def X_constraint(b, t):
+        return b.X[t] == b.PowerFCRCharge[t] + b.Pfeedout[t]
+    b.XConstraint = pyo.Constraint(t, rule=X_constraint)
 
     
 
