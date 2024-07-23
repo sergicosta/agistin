19fdf76ead1d7ee6083da9be92c3222bdf452059
Sergi Costa <sergi.costa.dilme@upc.edu>
2024-07-23 11:30:24 +0200

    feat(Turbines): added discrete turbines

--- main/Devices/Turbines.py
+++ main/Devices/Turbines.py
@@ -66,8 +66,8 @@ def Turbine(b, t, data, init_data):
     b.Qin  = pyo.Var(t, initialize=[-k for k in init_data['Q']], within=pyo.NonPositiveReals)
     b.Qout = pyo.Var(t, initialize=init_data['Q'], within=pyo.NonNegativeReals)
     b.H    = pyo.Var(t, initialize=init_data['H'], within=pyo.NonNegativeReals) 
-    b.Ph   = pyo.Var(t, initialize=init_data['Pe'], within=pyo.NonPositiveReals)
-    b.Pe   = pyo.Var(t, initialize=init_data['Pe'], within=pyo.NonPositiveReals)
+    b.Ph   = pyo.Var(t, initialize=init_data['Pe'], bounds=(-data['Pmax'], 0), within=pyo.NonPositiveReals)
+    b.Pe   = pyo.Var(t, initialize=init_data['Pe'], bounds=(-data['Pmax']*data['eff'], 0), within=pyo.NonPositiveReals)
     
     # Ports
     b.port_Qin  = Port(initialize={'Q': (b.Qin, Port.Extensive)})
@@ -79,6 +79,93 @@ def Turbine(b, t, data, init_data):
     def Constraint_Q(_b, _t):
         return _b.Qin[_t] == -_b.Qout[_t]
     b.c_Q = pyo.Constraint(t, rule = Constraint_Q)
+    
+    def Constraint_Ph(_b, _t):
+        return _b.Ph[_t] == 9810*_b.H[_t]*_b.Qin[_t]
+    b.c_Ph = pyo.Constraint(t, rule = Constraint_Ph)
+    
+    def Constraint_Pe(_b, _t):
+        return _b.Pe[_t] == _b.Ph[_t]*_b.eff
+    b.c_Pe = pyo.Constraint(t, rule = Constraint_Pe)
+
+    def Constraint_Pdim(_b, _t):
+        return _b.Pdim >= -_b.Pe[_t]
+    b.c_Pdim = pyo.Constraint(t, rule = Constraint_Pdim)
+    
+    
+    
+def DiscreteTurbine(b, t, data, init_data):
+    
+    """
+    Turbine.
+    
+    Sizes a new turbine.
+    
+    :param b: pyomo ``Block()`` to be set
+    :param t: pyomo ``Set()`` referring to time
+    :param data: data ``dict``
+    :param init_data: init_data ``dict``
+        
+    data
+         - 'eff': Efficiency of the turbine :math:`\eta`
+         - 'Pmax': Maximum allowed power :math:`P_{max}`
+         - 'Qmax': Maximum allowed flow :math:`Q_{max}`
+    
+    init_data
+         - 'Q': Flow :math:`Q(t)` as a ``list``
+         - 'H': Head :math:`H(t)` as a ``list``
+         - 'Pe': Electrical power :math:`P_e(t)` as a ``list``
+         
+    Pyomo declarations    
+        - Parameters: 
+            - eff 
+        - Variables: 
+            - Pdim bounded :math:`P_{dim} \ge 0`
+            - Qin (t) bounded :math:`Q_{in} \le 0`
+            - Qout (t) bounded :math:`Q_{out} \ge 0`
+            - H (t) bounded :math:`H \ge 0`
+            - Ph (t) bounded :math:`P_{h} \le 0`
+            - Pe (t) bounded :math:`P_{e} \le 0`
+        - Ports: 
+            - port_Qin @ Qin as ``Extensive``
+            - port_Qout @ Qout as ``Extensive``
+            - port_P @ P as ``Extensive``
+            - port_H @ H as ``Equality``
+        - Constraints: 
+            - c_Q: :math:`Q_{in}(t) = -Q_{out}(t)`
+            - c_Ph: :math:`P_h(t) = 9810\, H(t)\, Q_{in}`
+            - c_Pe: :math:`P_e(t) = P_h(t)\, \eta`
+            - c_Pdim: :math:`P_{dim}(t) \ge -P_e(t)`
+    """
+    
+    # Parameters
+    b.eff   = pyo.Param(initialize=data['eff'])
+    b.Pmax  = pyo.Param(initialize=data['Pmax'])
+    b.Qstep = pyo.Param(initialize=data['Pmax']/(9810*init_data['H'][0])/10)
+    
+    # Variables
+    b.Pdim = pyo.Var(initialize=0, bounds=(0, data['Pmax']), within=pyo.NonNegativeReals)
+    b.Qin  = pyo.Var(t, initialize=[-k for k in init_data['Q']], within=pyo.NonPositiveReals)
+    b.Qout = pyo.Var(t, initialize=init_data['Q'], within=pyo.NonNegativeReals)
+    b.H    = pyo.Var(t, initialize=init_data['H'], within=pyo.NonNegativeReals) 
+    b.Ph   = pyo.Var(t, initialize=init_data['Pe'], bounds=(-data['Pmax'], 0), within=pyo.NonPositiveReals)
+    b.Pe   = pyo.Var(t, initialize=init_data['Pe'], bounds=(-data['Pmax']*data['eff'], 0), within=pyo.NonPositiveReals)
+    b.Nq   = pyo.Var(t, initialize=0, bounds=(0,10), within=pyo.NonNegativeIntegers)
+    
+    # Ports
+    b.port_Qin  = Port(initialize={'Q': (b.Qin, Port.Extensive)})
+    b.port_Qout = Port(initialize={'Q': (b.Qout, Port.Extensive)})
+    b.port_P    = Port(initialize={'P': (b.Pe, Port.Extensive)})
+    b.port_H    = Port(initialize={'H': (b.H, Port.Equality)})
+    
+    # Constraints
+    def Constraint_Qin(_b, _t):
+        return _b.Qin[_t] == -_b.Nq[_t]*_b.Qstep
+    b.c_Qin = pyo.Constraint(t, rule = Constraint_Qin)
+    
+    def Constraint_Qout(_b, _t):
+        return _b.Qout[_t] == -_b.Qin[_t]
+    b.c_Qout = pyo.Constraint(t, rule = Constraint_Qout)
     
     def Constraint_Ph(_b, _t):
         return _b.Ph[_t] == 9810*_b.H[_t]*_b.Qin[_t]
