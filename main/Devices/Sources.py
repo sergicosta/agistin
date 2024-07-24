84aa0e72f4cd3d04fe03612e0931d8344e388a9a
Sergi Costa <sergi.costa.dilme@upc.edu>
2024-07-24 11:13:35 +0200

    perf(Sources): changed from constraints to bounds

--- main/Devices/Sources.py
+++ main/Devices/Sources.py
@@ -47,17 +47,21 @@ def Source(b, t, data, init_data=None):
     b.Qin = pyo.Var(t, initialize=[-k for k in data['Q']], within=pyo.Reals)
     b.Qout = pyo.Var(t, initialize=data['Q'], within=pyo.Reals)
     
+    for k in range(len(t)):
+        b.Qin[k].bounds = (-Q[k],-Q[k])
+        b.Qout[k].bounds = (Q[k],Q[k])
+    
     # Ports
     b.port_Qin = Port(initialize={'Q': (b.Qin, Port.Extensive)})
     b.port_Qout = Port(initialize={'Q': (b.Qout, Port.Extensive)})
     
     # Constraints
-    def Constraint_Qin(_b, _t):
-        return _b.Qin[_t] == -_b.Q[_t]
-    b.c_Qin = pyo.Constraint(t, rule=Constraint_Qin)
-    def Constraint_Qout(_b, _t):
-        return _b.Qout[_t] == _b.Q[_t]
-    b.c_Qout = pyo.Constraint(t, rule=Constraint_Qout)
+    # def Constraint_Qin(_b, _t):
+    #     return _b.Qin[_t] == -_b.Q[_t]
+    # b.c_Qin = pyo.Constraint(t, rule=Constraint_Qin)
+    # def Constraint_Qout(_b, _t):
+    #     return _b.Qout[_t] == _b.Q[_t]
+    # b.c_Qout = pyo.Constraint(t, rule=Constraint_Qout)
 
 
 def VarSource(b, t, data, init_data):
