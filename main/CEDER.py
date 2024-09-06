6e878a27b804c2292bfebd545b5a3c5ec93cb355
paugmotilla <paugmotilla@gmail.com>
2024-09-06 09:08:40 +0200

    CEDER plots modified

--- main/CEDER.py
+++ main/CEDER.py
@@ -2,16 +2,11 @@
     AGISTIN - EXAMPLE CEDER
     
     3 Reservoirs wihout irrigation connected to a battery and a PV system.
-    Giving services to loads.
+    The system is supposed to service electrical loads.
     
-    Authors: Sergi Costa Dilmé (CITCEA-UPC), Juan Carlos Olives-Camps (CITCEA-UPC), Paula Muñoz Peña (CITCEA-UPC)
+    Authors: Sergi Costa Dilmé (CITCEA-UPC), Juan Carlos Olives-Camps (CITCEA-UPC), Paula Muñoz Peña (CITCEA-UPC), Pau García (CITCEA-UPC)
 """
 
-"""
-RUGOSITAT:
-    - PE-100: Ka = 0.050 mm
-    - PVC-O: Ka = 0.050 mm
-"""
 
 from pyomo.util.infeasible import log_infeasible_constraints
 import logging
@@ -145,25 +140,13 @@ summer_var = df_summer.groupby('Hour').var()
 
 #%%
 
+# Take in account the season of the optimization (winter or summer)
+
 date = '2023-07-02'
 ho = 0
 hf = 24
 df_meteo = pd.DataFrame()
-# df_meteo ['Irr'] = df_data['Irr'][(df_data.index == date) & (df_data['Hour'] >= ho) & (df_data['Hour'] <= hf)]
-# df_load = - df_data['CT_Edificio1'][(df_data.index == date) & (df_data['Hour'] >= ho) & (df_data['Hour'] <= hf)]
-# df_grid = pd.read_csv('data/costs/PVPC_jul.csv').iloc[ho:(hf)].reset_index(drop=True)
-df_grid =  pd.read_csv('data/costs/PVPC_jan.csv').iloc[ho:(hf)].reset_index(drop=True)
-
-
-
-
-
-# df_concat = pd.concat([summer_day_mean, winter_day_mean])
-# df_meteo = pd.DataFrame()
-
-# df_meteo['Irr'] = df_concat['Irr']
-# df_load = df_concat['CT_Edificio1']
-# df_grid = pd.concat([df_grid_Jul,df_grid_Jan])
+df_grid =  pd.read_csv('data/costs/PVPC_jul.csv').iloc[ho:(hf)].reset_index(drop=True)
 df_meteo['Irr']  = winter_day_mean['Irr']
 df_load = - winter_day_mean['CT_Edificio1']
 
@@ -256,7 +239,7 @@ init_R2 = {'Q':[0]*T, 'W':[0.25e3]*T}
 Reservoir(m.Reservoir2s, m.ts, data_R2, init_R2)
 # Reservoir(m.Reservoir2w, m.tw, data_R2, init_R2)
 
-data_c1 = {'K':300, 'Qmax':0.5} # canal
+data_c1 = {'K':300*0.2, 'Qmax':0.5} # canal
 init_c1 = {'Q':[0]*T, 'H':[77]*T, 'H0':[77]*T, 'zlow':[0]*T, 'zhigh':[77]*T}
 Pipe(m.Pipe01s, m.ts, data_c1, init_c1)
 Pipe(m.Pipe01Ts, m.ts, data_c1, init_c1)
@@ -264,7 +247,7 @@ Pipe(m.Pipe01Ts, m.ts, data_c1, init_c1)
 # Pipe(m.Pipe01Tw, m.tw, data_c1, init_c1)
 
 
-data_c2 = {'K':300, 'Qmax':0.5} # canal
+data_c2 = {'K':300*0.2, 'Qmax':0.5} # canal
 init_c2 = {'Q':[0]*T, 'H':[67]*T, 'H0':[67]*T, 'zlow':[2]*T, 'zhigh':[67]*T}
 Pipe(m.Pipe02s, m.ts, data_c2, init_c2)
 # Pipe(m.Pipe02w, m.tw, data_c2, init_c2)
@@ -533,7 +516,7 @@ df_out, df_param, df_size = get_results(file=file, instance=instance, results=re
 
 #Plots
 
-file = './results/CEDER/Case3_24hWinter'
+file = './results/CEDER/Case3_24hSummer'
 df_out = pd.read_csv(file+'.csv').drop('Unnamed: 0',axis=1)
 df_param = pd.read_csv(file+'_param.csv').drop('Unnamed: 0',axis=1)
 df_size = pd.read_csv(file+'_size.csv').drop('Unnamed: 0',axis=1)
@@ -547,37 +530,33 @@ df_melted = pd.melt(df_out, id_vars=['t'], value_vars=['Pump1s.Pe', 'Pump2s.Pe',
                     var_name='Variable', value_name='Value')
 df_melted['Value'] = df_melted['Value'] / 1000
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={'height_ratios': [3, 1]})
 
-# Primer subplot: gráfico de barras apiladas
+
+#Power balance plot
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={'height_ratios': [3, 1]})
 df_pivot = df_melted.pivot(index='t', columns='Variable', values='Value').fillna(0)
 colors = sns.color_palette("colorblind", len(df_pivot.columns))
 
-# Dividir los datos positivos y negativos
 df_positive = df_pivot.clip(lower=0)
 df_negative = df_pivot.clip(upper=0)
 
-# Plotear los valores positivos
 bottom_pos = np.zeros(len(df_positive))
 for i, col in enumerate(df_positive.columns):
     ax1.bar(df_positive.index, df_positive[col], bottom=bottom_pos, label=col, color=colors[i])
     bottom_pos += df_positive[col]
 
-# Plotear los valores negativos
 bottom_neg = np.zeros(len(df_negative))
 for i, col in enumerate(df_negative.columns):
     ax1.bar(df_negative.index, df_negative[col], bottom=bottom_neg, color=colors[i])
     bottom_neg += df_negative[col]
 
-# Línea horizontal en y=0
 ax1.axhline(0, color='black', linewidth=1)
 
-# Configurar el título y las etiquetas de los ejes del primer subplot
 ax1.set_title('Generated and Consumed Power', fontsize=18)
 ax1.set_xlabel('Time', fontsize=14)
 ax1.set_ylabel('Power [kW]', fontsize=14)
 
-# Segundo subplot: gráfico de línea de costos
 ax2.plot(df_out['t'], l_costs, color='black', marker='o', linestyle='dashed', linewidth=2, label='PVPC')
 ax2.plot(df_out['t'], df_grid['Excedentes'], color='red', marker='o', linestyle='dashed', linewidth=2, label='Selling price')
 
@@ -585,322 +564,66 @@ ax2.set_ylabel('Price [€]', color='black', fontsize=14)
 ax2.set_xlabel('Time', fontsize=14)
 ax2.tick_params(axis='y', labelcolor='black')
 
-# Obtener los handles y labels de cada eje
 handles1, labels1 = ax1.get_legend_handles_labels()
 handles2, labels2 = ax2.get_legend_handles_labels()
 
-# Crear las leyendas y posicionarlas adecuadamente
-legend1 = ax1.legend(handles1[:len(handles1)//2], labels1[:len(labels1)//2], loc='lower right', fontsize=10, bbox_to_anchor=(0.18, 0))
-legend2 = ax1.legend(handles1[len(handles1)//2:], labels1[len(labels1)//2:], loc='lower right', fontsize=10, bbox_to_anchor=(0.38, 0))
-legend3 = ax2.legend(handles2, labels2, loc='lower right', fontsize=10, bbox_to_anchor=(1, 1))
+legend1 = ax1.legend(handles1[:len(handles1)//2], labels1[:len(labels1)//2], loc='lower right', fontsize=11, bbox_to_anchor=(0.2, 0))
+legend2 = ax1.legend(handles1[len(handles1)//2:], labels1[len(labels1)//2:], loc='lower right', fontsize=11, bbox_to_anchor=(0.98, 0))
+legend3 = ax2.legend(handles2, labels2, loc='lower right', fontsize=11, bbox_to_anchor=(1, 1))
 
-# Añadir las leyendas al primer subplot
 ax1.add_artist(legend1)
 ax1.add_artist(legend2)
 ax1.set_ylim(-45, 45)
 
-# Ajustar el espacio entre los subplots
 plt.tight_layout()
 
-# Mostrar el gráfico
 plt.show()
 
-# Guardar el gráfico
+# Plot save
 plt.rcParams['savefig.format']='pdf'
 plt.savefig(file + 'Electrical', dpi=300)
    
 df = df_out
 colors = sns.color_palette("colorblind", len(df_pivot.columns))
 
-fig = plt.figure(figsize=(6, 4))
+
+# 2n Plot, Volume and flow
+fig = plt.figure(figsize=(8, 6))
+
 fig.add_subplot(2,1,1)
 ax1 = sns.lineplot(data=df, x=df.index,y='Reservoir1s.W', label='R1', color=cbcolors[2], marker='^')
 sns.lineplot(data=df, x=df.index,y='Reservoir0s.W', label='R0', color=cbcolors[3], marker='v')
 sns.lineplot(data=df, x=df.index,y='Reservoir2s.W', label='R2', color=cbcolors[0], marker='x')
 
-plt.xlabel('Time',fontsize=14)
-plt.ylabel('Volume[m3]',fontsize=14)
+plt.title("Reservoirs level", fontsize=14)
+plt.xlabel('Time', fontsize=14)
+plt.ylabel('Volume [m3]', fontsize=14)
+
 ax2 = fig.add_subplot(2,1,2)
-df.plot(y=['Pump1s.Qout', 'Turb1s.Qout', 'Pump2s.Qout'], kind='bar', stacked=False, ylabel='Q', ax=ax2, 
-        color=[cbcolors[2],cbcolors[1],cbcolors[0]], edgecolor='dimgray', label=['P1','T1','P2'], sharex=True)
+df.plot(
+    y=['Pump1s.Qout', 'Turb1s.Qout', 'Pump2s.Qout'], 
+    kind='bar', 
+    stacked=False, 
+    ylabel='Q', 
+    ax=ax2, 
+    color=[cbcolors[2], cbcolors[1], cbcolors[0]], 
+    edgecolor='dimgray', 
+    label=['Pump1','Pump2','Turbine'], 
+    sharex=True, 
+    width= 1
+)
+
 ax2.legend()
-plt.xlabel('Time',fontsize = 12)
-plt.ylabel('Flow [m3/s]',fontsize = 12)
+plt.xlabel('Time', fontsize=14)
+plt.ylabel('Flow [m3/s]', fontsize=14)
+
 plt.tight_layout()
 plt.show()
 
-plt.rcParams['savefig.format']='pdf'
+# Plot save
+plt.rcParams['savefig.format'] = 'pdf'
 plt.savefig(file + '_VQ', dpi=300)
 
 
 
-#%%
-#Power 
 
-fig = plt.figure(figsize=(5, 3))
-ax1 = fig.add_subplot(1,1,1)
-
-# Gráfico de barras en el eje izquierdo (ax1)
-df['Load.P'] = -df['Load.P']
-df.plot(y=['Pump1.Pe', 'PV.P', 'Grid.P', 'Turb1.Pe','Load.P','Pump2.Pe'], kind='bar', stacked=True, ax=ax1,
-        color=['lightgrey', 'lightgrey', 'dimgrey', 'k', 'lightgrey','lightgrey'], edgecolor='dimgray')
-
-bars = ax1.patches
-patterns = ('////', '\\\\', None, None, 'xxxxx', None,'oooo')
-hatches = [p for p in patterns for i in range(len(df))]
-for bar, hatch in zip(bars, hatches):
-    bar.set_hatch(hatch)
-
-ax1.legend(['$P_{p1}$', '$P_{PV}$', '$P_{g}$', '$P_{t1}$', '$P_{load}$','$P_{p2}$'], loc='upper center', bbox_to_anchor=(0.525, -0.2),
-           fancybox=False, shadow=False, ncol=4)
-ax1.axhline(0, color='k')
-# Crear el segundo eje y gráfico de línea en el eje derecho (ax2)
-ax2 = plt.twinx()
-sns.lineplot(df, ax=ax2, x=range(hf-ho), y='l_cost', color='blue', linestyle='-', marker='o')  # Ajusta 'l_cost' según tus datos
-ax2.set_xticks(range(hf-ho), labels=df.index, rotation=0)
-ax2.set_ylabel('Cost')
-ax2.legend(['Cost'], loc='upper center', bbox_to_anchor=(0.525, -0.25), fancybox=False, shadow=False)
-
-# ax2 = plt.twinx()
-# sns.lineplot(df ,x=df.index,y='l_cost', color='k', marker='.',ax=ax2)
-# ax2.set_xticks(df.index, labels=df.index, rotation=0)
-
-plt.ylabel('Price')
-plt.xlabel('Time')
-plt.title('Power consumption')
-plt.tight_layout()
-plt.show()
-plt.rcParams['savefig.format']='pdf'
-plt.savefig(file + '_P', dpi=300)
-
-
-
-#%% PLOTS
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib import gridspec
-
-
-labels_hours = ['0','','','','','','6','','','','','','12','','','','','','18','','','','','23']
-
-cbcolors = sns.color_palette('colorblind')
-
-file = 'Case2_tstw/ISGT'
-
-df_meteo_aug = pd.read_csv('data/meteo/LesPlanes_meteo_hour_aug.csv').head(24)
-df_cons_aug = pd.read_csv('data/irrigation/LesPlanes_irrigation_aug.csv').head(24)
-df_grid_aug = pd.read_csv('data/costs/PVPC_aug.csv').head(24)
-df_meteo_jan = pd.read_csv('data/meteo/LesPlanes_meteo_hour_jan.csv').head(24)
-df_cons_jan = pd.read_csv('data/irrigation/LesPlanes_irrigation_jan.csv').head(24)
-df_grid_jan = pd.read_csv('data/costs/PVPC_jan.csv').head(24)
-
-
-df_param = pd.read_csv('results/ISGT/'+file+'_param.csv')
-w_lim = df_param['Reservoir1w.Wmin']
-
-df = pd.read_csv('results/ISGT/'+file+'.csv')
-df['PVw.Pf'] = -df_meteo_jan['Irr']/1000*215.28e3*0.98
-df['PVs.Pf'] = -df_meteo_aug['Irr']/1000*215.28e3*0.98
-
-
-if 'Turb1s.Qout' in df.columns:
-    df['Rev1w.Qout'] = df['Pump1w.Qout'] - df['Turb1w.Qout']
-    df['Rev1w.Pe'] = df['Pump1w.Pe'] + df['Turb1w.Pe']
-    df['Rev1s.Qout'] = df['Pump1s.Qout'] - df['Turb1s.Qout']
-    df['Rev1s.Pe'] = df['Pump1s.Pe'] + df['Turb1s.Pe']
-else:
-    df['Rev1w.Qout'] = df['Pump1w.Qout']
-    df['Rev1w.Pe'] = df['Pump1w.Pe']
-    df['Rev1s.Qout'] = df['Pump1s.Qout']
-    df['Rev1s.Pe'] = df['Pump1s.Pe']
-
-df['Irrigation1w.Qin'] = -df['Irrigation1w.Qout'] # no se per que passa pero Qin a vegades es reseteja a 0. La resta tot be
-df['Irrigation1s.Qin'] = -df['Irrigation1s.Qout']
-
-for c in df.columns:
-    df[c] = df[c].apply(lambda x: 0 if (x>-1e-5 and x<1e-5) else x)
-
-df_W = df[[col for col in df.columns if 'w.' in col]]
-df_S = df[[col for col in df.columns if 's.' in col]]
-df_W['t'] = df_W.index
-df_S['t'] = df_S.index
-df_W['PVPC'] = df_grid_jan['PVPC']
-df_S['PVPC'] = df_grid_aug['PVPC']
-df_W['Excedentes'] = df_grid_jan['Excedentes']
-df_S['Excedentes'] = df_grid_aug['Excedentes']
-
-df_W.columns = [col.replace('w.','.') for col in df_W.columns]
-df_S.columns = [col.replace('s.','.') for col in df_W.columns]
-
-#%%
-i=0
-season=['S','W']
-for df in [df_S, df_W]:
-    
-    fig = plt.figure(figsize=(3.4, 2.1))
-    plt.rcParams['axes.spines.right'] = True
-    ax1 = fig.add_subplot(1,1,1)
-    # df.apply(lambda x: x/1000).plot(y=['PV.Pf'], kind='bar', ax=ax1, stacked=False, ylabel='P (kW)', 
-    #                             color='#F0F0F0', alpha=1, edgecolor='#808080')
-    # df.apply(lambda x: x/1000).plot(y=['PV.P','Pump2.Pe','Rev1.Pe'], kind='bar', stacked=True, ax=ax1, ylabel='P (kW)',
-    #                                 color=['lightgrey','#606060','#454545'], edgecolor='#808080')
-    df.apply(lambda x: x/1000).plot(y=['PV.Pf'], kind='bar', ax=ax1, stacked=False, ylabel='P (kW)', 
-                                color='#C0E3C0', alpha=1, edgecolor=None)
-    df.apply(lambda x: x/1000).plot(y=['PV.P','Pump2.Pe','Pump1.Pe','Turb1.Pe'], kind='bar', stacked=True, ax=ax1, ylabel='P (kW)',
-                                color=[cbcolors[2],cbcolors[0],cbcolors[1]], edgecolor=None)
-    ax1.axhline(0,color='k')
-    ax1.set_ylim(-200,200)
-    bars = ax1.patches
-    # patterns =(None, None,'/////',None)
-    patterns =(None, None, None, None)
-    hatches = [p for p in patterns for i in range(len(df))]
-    for bar, hatch in zip(bars, hatches):
-        bar.set_hatch(hatch)
-    ax1.legend(['_','$\hat{P}_{PV}$','$P_{PV}$','$P_{p,PV}$','$P_{p,g}$'],loc='upper center', bbox_to_anchor=(0.5, -0.2),
-          fancybox=False, shadow=False, ncol=4)
-    
-    ax2 = plt.twinx()
-    ax2.set_ylim(45,300)
-    sns.lineplot(df,x='t' ,y='PVPC', ax=ax2, color='tab:red')#, label='Buy')
-    sns.lineplot(df,x='t' ,y='Excedentes', ax=ax2, color='tab:red', linestyle='dashed')#, label='Buy')
-    ax2.set_xticks(range(24), labels=labels_hours, rotation=90)
-    
-    plt.ylabel('Price (€/MWh)')
-    plt.xlabel('Hour')
-    # plt.title('Power consumption')
-    plt.show()
-    plt.tight_layout()
-    
-    plt.rcParams['savefig.format']='pdf'
-    plt.savefig('results/ISGT/' + file + season[i] + '_P', dpi=300)
-    plt.rcParams['savefig.format']='svg'
-    plt.savefig('results/ISGT/' + file + season[i] + '_P', dpi=300)
-    
-    
-    
-    fig = plt.figure(figsize=(3.4, 2))
-    plt.rcParams['axes.spines.right'] = False
-    gs = gridspec.GridSpec(2,1,height_ratios=[2,1])
-    
-    ax1 = fig.add_subplot(gs[0])
-    ax1.set_ylim(-0.2,0.2)
-    # df.plot(y=['Pump2.Qout','Irrigation1.Qin','Rev1.Qout'], kind='bar', stacked=True, ax=ax1, ylabel='Q (m$^3$/s)',
-    #         color=['#606060','lightgrey','#454545'], edgecolor='#808080')
-    df.plot(y=['Pump2.Qout','Irrigation1.Qin','Rev1.Qout'], kind='bar', stacked=True, ax=ax1, ylabel='Q (m$^3$/s)',
-            color=[cbcolors[0],cbcolors[2],cbcolors[1]], edgecolor=None)
-    bars = ax1.patches
-    patterns =(None,None,None)
-    hatches = [p for p in patterns for i in range(len(df))]
-    for bar, hatch in zip(bars, hatches):
-        bar.set_hatch(hatch)
-    ax1.legend(['$Q_{p,PV}$', '$Q_{irr}$', '$Q_{p,g}$'], ncols=3)
-    ax1.axhline(0,color='k')
-    ax1.set_xticks(range(24), labels=labels_hours, rotation=90)
-    
-    ax2 = fig.add_subplot(gs[1],sharex=ax1)
-    ax2.figsize=(3.4,1)
-    ax2.set_ylim(7500,14000)
-    ax2.set_yticks([7500,10000,13000])
-    ax2.axhline(w_lim,color='#AFAFAF', alpha=1)
-    ax2.axhline(13000,color='#AFAFAF', alpha=1)
-    ax2.axhline(10000*0.95, color='#AFAFAF', linestyle='--', alpha=1)
-    ax2.axhline(10000*1.05, color='#AFAFAF', linestyle='--', alpha=1)
-    sns.lineplot(df, x='t', y='Reservoir1.W', ax=ax2, color='tab:red')
-    
-    plt.ylabel('R1 Volume (m$^3$)')
-    plt.xlabel('Hour')
-    plt.xticks(range(24),labels=labels_hours,rotation=90)
-    plt.tight_layout()
-    plt.show()
-
-    plt.rcParams['savefig.format']='pdf'
-    plt.savefig('results/ISGT/' + file + season[i] + '_Q', dpi=300)
-    plt.rcParams['savefig.format']='svg'
-    plt.savefig('results/ISGT/' + file + season[i] + '_Q', dpi=300)
-    
-    i=i+1
-
-
-
-# df = pd.read_csv('data/Analogiques.csv')
-# df['date'] = pd.to_datetime(df['Fecha'], format='%Y/%m/%d')
-# df['month'] = df['date'].dt.month
-# df['weekday'] = df['date'].dt.dayofweek
-# df['Volum bassa'] = df['Nivell bassa']*13000/4.5
-# df['Qdiff'] = df['Volum bassa'] - df['Volum bassa'].shift(1) 
-# df['Qreg'] = df['Cabal impulsio'] - df['Qdiff']
-# df['Qreg'] = df['Qreg'].apply(lambda x: 0 if x<0 else x)
-# df['Qreg_lag'] = df['Qreg'].shift(1)
-# df.loc[df['Qreg']>300,'Qreg'] = df['Qreg_lag']
-
-# df['Season'] = df['month']/4
-# df['Season'] = df['Season'].astype(int)
-# fig = plt.figure(figsize=(3.4, 2.3))
-# sns.lineplot(df, x='hour', y='Qreg', style='Season', color='tab:grey')
-# plt.legend(labels=['Winter','_','Spring','_','Summer','_','Fall','_'], ncol=1, loc='upper right')
-# plt.xlabel('Hour')
-# plt.ylabel('Irrigation demand (m$^3$/h)')
-# plt.xticks(range(24),labels=labels_hours, rotation=90)
-# plt.xlim([0,24])
-# plt.ylim([0,250])
-# plt.tight_layout()
-# plt.subplots_adjust(left=0.15, right=1, top=0.95, bottom=0.18)
-# plt.show()
-# plt.rcParams['savefig.format']='pdf'
-# plt.savefig('results/ISGT/Irrigation_season', dpi=300)
-
-# df['Winter'] = df['date'].apply(lambda x: 0 if (x.month in [3,4,5,6,7,8]) else 1)
-# fig = plt.figure(figsize=(3.4, 1.3))
-# sns.lineplot(df, x='hour', y='Qreg', style='Winter', hue='Winter', palette=[cbcolors[1],cbcolors[0]], errorbar=('ci',90))
-# plt.legend(labels=['S','_','W','_'], ncol=1)
-# plt.xlabel('Time (h)')
-# plt.ylabel('Demand (m$^3$/h)')
-# plt.xticks(range(24),labels=labels_hours, rotation=90)
-# plt.yticks([0,100,200])
-# # plt.ylim([0,200])
-# plt.xlim([0,24])
-# plt.tight_layout()
-# plt.subplots_adjust(left=0.15, right=1, top=0.95, bottom=0.3)
-# plt.show()
-# plt.rcParams['savefig.format']='pdf'
-# plt.savefig('results/ISGT/Irrigation_WS', dpi=300)
-# plt.rcParams['savefig.format']='svg'
-# plt.savefig('results/ISGT/Irrigation_WS', dpi=300)
-
-#%%
-df = df_melted
-df['PVPC'] = l_costs
-df['Excedentes'] = l_costs/3 
-labels_hours = ['0','','','','','','6','','','','','','12','','','','','','18','','','','22']
-
-fig = plt.figure(figsize=(3.4, 1.8))
-plt.rcParams['axes.spines.right'] = True
-ax1 = fig.add_subplot(1,1,1)
-
-df.apply(lambda x: x/1000).plot(y=['PV.P','Turb.Pe','Grid.P'], kind='bar', stacked=True, ax=ax1, ylabel='P (kW)',
-                            color=[cbcolors[2],cbcolors[0],cbcolors[1]], edgecolor=None)
-
-
-ax1.axhline(0,color='k')
-ax1.set_ylim(-200,200)
-bars = ax1.patches
-patterns =(None, None, None, None)
-hatches = [p for p in patterns for i in range(len(df))]
-for bar, hatch in zip(bars, hatches):
-    bar.set_hatch(hatch)
-ax1.legend(['_','$\hat{P}_{PV}$','$P_{PV}$','$P_{p,PV}$','$P_{p,g}$'],loc='upper center', bbox_to_anchor=(0.5, -0.18),
-      fancybox=False, shadow=False, ncol=4)
-
-ax2 = plt.twinx()
-ax2.set_ylim(45,300)
-sns.lineplot(df,x='t' ,y='PVPC', ax=ax2, color='tab:red')#, label='Buy')
-sns.lineplot(df,x='t' ,y='Excedentes', ax=ax2, color='tab:red', linestyle='dashed')#, label='Buy')
-ax2.set_xticks(range(23), labels=labels_hours, rotation=90)
-ax2.text(24.5,0.1,'Time (h)')
-
-plt.ylabel('Price (€/MWh)')
-plt.xlabel('Hour')
-# plt.title('Power consumption')
-plt.subplots_adjust(left=0.17, right=0.82, top=0.97, bottom=0.3)
-plt.show()
