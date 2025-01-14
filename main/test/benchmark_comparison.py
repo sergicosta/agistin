a3ea4733cab03d715069e8bc12d7a41baf1013b1
sergicosta <sergi.costa.dilme@upc.edu>
2025-01-14 16:51:28 +0000

    test(Benchmark): improved plots to show base as red dashed line

--- main/test/benchmark_comparison.py
+++ main/test/benchmark_comparison.py
@@ -20,7 +20,7 @@ cbcolors = sns.color_palette('colorblind')
 
 df = pd.DataFrame()
 
-for f in ['df_base.csv','df_full_bb_eff.csv', 'df_full_ecp.csv','df_full_ecp_eff.csv']:
+for f in ['df_base.csv','df_full_bb.csv','df_full_bb_eff.csv', 'df_full_ecp.csv','df_full_ecp_eff.csv', 'df_full_ecp_eqpump.csv']:
     df = pd.concat([df, pd.read_csv(f)])
 
 
@@ -47,15 +47,31 @@ del df_a, df_b, lst, f
 
 #%% Plots
 
+# execution time
 plt.figure()
-sns.boxplot(data=df, x='case', y='time',hue='test', palette=cbcolors)
-# plt.yscale('log')
-plt.ylim(0)
+sns.boxplot(data=df.query('test!=\'Base\''), x='case', y='time', hue='test', palette=cbcolors, linewidth=0.5)
+n=0
+for e in df.query('test==\'Base\'')['time']:
+    plt.axvline(n+0.5, color='k', linewidth=0.5)
+    plt.hlines(y=e,xmin=n-0.5, xmax=n+0.5, color='tab:red',linestyle='--')
+    n=n+1
+plt.yscale('log')
 plt.ylabel('Execution time (s)')
+plt.xlim(-0.5)
+plt.legend(loc='lower right')
 plt.show()
 
+# objective function
 plt.figure()
-sns.barplot(data=df, x='case', y='objective',hue='test', palette=cbcolors)
+sns.barplot(data=df.query('test!=\'Base\''), x='case', y='objective',hue='test', palette=cbcolors)
+n=0
+N = len(df.query('test==\'Base\'')['objective'])
+for e in df.query('test==\'Base\'')['objective']:
+    plt.axvline(n+0.5, color='k', linewidth=0.5)
+    plt.hlines(y=e,xmin=n-0.5, xmax=n+0.5, color='tab:red',linestyle='--')
+    n=n+1
 plt.axhline(0,color='k')
 plt.ylabel('Objective value')
+plt.xlim(-0.5)
+plt.legend(loc='lower right')
 plt.show()
