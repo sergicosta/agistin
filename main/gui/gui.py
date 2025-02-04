adc5b01e79d5886b83529a5b8907c80cd7c6f84e
sergicosta <sergi.costa.dilme@upc.edu>
2025-02-04 14:45:18 +0000

    feat(gui): added selection and improved deleting. Closes #25 and fixes #26 and fixes #27

--- main/gui/gui.py
+++ main/gui/gui.py
@@ -9,15 +9,13 @@ import tkinter as tk
 
 
 # GLOBAL VARIABLES
+# TODO: reduce amount of global variables
+devices_dict = dict()
 
-devices_dict = read_devices()
-
-blocks_dict = {} #{tag:{'id':int, 'device':str, 'pos':(x,y,xf,yf)}, ...}
-arcs_dict = {} #{tag:{'from':,'to':}, ...}
+blocks_dict = {} #{tag:{'type':str}, ...}
+arcs_dict = {} #{tag:{'from':BlockName.port_Name,'to':BlockName.port_Name}, ...}
 last_block_id = {d: 0 for d in devices_dict}
 last_arc_id = 0
-blocks_list = ['']
-arcs_list = ['']
 block_from = ''
 block_to = ''
 
@@ -29,61 +27,54 @@ is_drawing = False
 
 # Read and return the available devices from the main/Devices folder
 def read_devices():
-    # dev_list = []
     dev_dict = dict()
     dev_name = ""
     for n in os.listdir("../Devices"):
         if "_" not in n:
             with open("../Devices/"+n,'r') as f:
                 for l in f:
+                    # device name
                     if l[0:3] == "def":
-                        dev_name = l[4:].split("(")[0]
-                        # dev_list.append(dev_name)
-                        dev_dict[dev_name] = []
-                    if "Port(" in l and dev_name!="":
-                        dev_dict[dev_name].append(l.split(".")[1].split()[0])
+                        dev_name = l[4:].split("(")[0] # def DeviceName(args):
+                        dev_dict[dev_name] = {'Ports':[]}
+                    # device ports
+                    if "Port(" in l and dev_name!="": # b.port_name = Port(intialize=...):
+                        dev_dict[dev_name]['Ports'].append(l.split(".")[1].split()[0])
     return dev_dict
 
 # Binds canvas functions to cursor event depending on selection
 def set_current_canvas_function(func):
     global is_drawing, blocks_dict
+    
+    # unbind everything
     canvas.unbind('<Button-1>')
     canvas.unbind('<ButtonRelease-1>')
+    is_drawing = False
     
-    if func == 'arc':
-        is_drawing = False
+    # place arc
+    if func == 'sel':
+        canvas.config(cursor='top_left_arrow')
+        canvas.bind('<Button-1>', select_item)
+        
+    elif func == 'arc':
         canvas.config(cursor='cross')
         canvas.bind('<Button-1>', draw_arc)
     
+    # place block
     elif func == 'block':
         canvas.config(cursor='dotbox')
         canvas.bind('<ButtonRelease-1>', draw_block)
 
-# Refresh current blocks placed on canvas
-def resfresh_blocks_list():
-    global blocks_list
-    
-    blocks_list = list(blocks_dict.keys())
-    delete_menu_selection.set(blocks_list[0])
-    
-    delete_menu["menu"].delete(0, "end")
-    
-    for b in blocks_list:
-        delete_menu["menu"].add_command(label=b, command=tk._setit(delete_menu_selection, b))
-        
-# Refresh current arcs placed on canvas
-def resfresh_arcs_list():
-    global arcs_list
-    
-    arcs_list = list(arcs_dict.keys())
-    delete_menu2_selection.set(arcs_list[0])
-    
-    delete_menu2["menu"].delete(0, "end")
-    
-    for a in arcs_list:
-        delete_menu2["menu"].add_command(label=a, command=tk._setit(delete_menu2_selection, a))
-             
-# Place an arc on canvas   
+# Selects an item on canvas
+def select_item(event):
+    item = canvas.gettags("current")
+    if len(item)>0:
+        item = item[0].replace("_txt","")
+        item_selection.set(item)
+        item_selection_label.config(text=item)
+
+# Place an arc on canvas
+# TODO: (issue #28) fix error when selecting something that is not a Block -> do not allow
 def draw_arc(event):
     global is_drawing, init_pos, arcs_dict, last_arc_id, block_from, block_to
     
@@ -96,23 +87,27 @@ def draw_arc(event):
         txt = canvas.create_text((event.x+init_pos[0])/2, (event.y+init_pos[1])/2, tag=arc_name+'_txt')
         canvas.insert(txt, 10, arc_name)
         
-        block_to = canvas.gettags("current")[0].replace("_txt","").replace("block_","")
-        port_to = tk.simpledialog.askstring(title=block_to, parent=window, prompt=str(devices_dict[blocks_dict[block_to]['type']]))
-        block_to = block_to+'.'+port_to
+        block_to = canvas.gettags("current")[0].replace("_txt","")
+        block_to = show_dialog_ports(block_to)
         
         init_pos = [0,0]
         is_drawing = False
         
         arcs_dict[arc_name]={'from':block_from, 'to':block_to}
-        resfresh_arcs_list()
         
     else: # start
         is_drawing = True
         init_pos[0], init_pos[1] = event.x, event.y
-        block_from = canvas.gettags("current")[0].replace("_txt","").replace("block_","")
-        port_from = tk.simpledialog.askstring(title=block_from, parent=window, prompt=str(devices_dict[blocks_dict[block_from]['type']]))
-        block_from = block_from+'.'+port_from
-    
+        block_from = canvas.gettags("current")[0].replace("_txt","")
+        block_from = show_dialog_ports(block_from)
+
+def show_dialog_ports(block_name):
+    device_type = blocks_dict[block_name]['type']
+    port_name = tk.simpledialog.askstring(title=block_name, parent=window, 
+                                        prompt=str(devices_dict[device_type]['Ports']))
+    block_name = block_name+'.'+port_name
+    return block_name
+
 # Place a block on canvas   
 def draw_block(event):
     global code_blocks, blocks_dict, last_block_id
@@ -120,35 +115,30 @@ def draw_block(event):
     block_name = devices_menu_selection.get() + str(last_block_id[devices_menu_selection.get()])
     last_block_id[devices_menu_selection.get()] = last_block_id[devices_menu_selection.get()]+1
     
-    txt = canvas.create_text(event.x, event.y, tag='block_'+block_name+'_txt')
+    txt = canvas.create_text(event.x, event.y, tag=block_name+'_txt')
     canvas.insert(txt, 10, block_name)
-    canvas.create_rectangle(event.x-10, event.y-10, event.x+10, event.y+10, tag='block_'+block_name, activefill="red")
+    canvas.create_rectangle(event.x-10, event.y-10, event.x+10, event.y+10, tag=block_name, activefill="red")
     
     # TODO: (issue #21) add full info to dict
     blocks_dict[block_name]={'type':devices_menu_selection.get()}
         
-    resfresh_blocks_list()
-
+    
 # Delete block/arc
 # TODO: (issue #25) better delete for arcs and blocks
 # TODO: (issue #26) error when no blocks or arcs left
 # TODO: (issue #27) error when last block/arc when setting var, since list is empty --> delete_menu2_selection.set(arcs_list[0])
-def delete_block(block_name):
-    global blocks_dict
+def delete_item(item_name):
+    global blocks_dict, arcs_dict
     
-    canvas.delete("block_"+block_name)
-    canvas.delete("block_"+block_name+'_txt')
-    blocks_dict.pop(block_name)
-
-    resfresh_blocks_list()
-def delete_arc(arc_name):
-    global arcs_dict
-    
-    canvas.delete(arc_name)
-    canvas.delete(arc_name+'_txt')
-    arcs_dict.pop(arc_name)
-
-    resfresh_arcs_list()
+    canvas.delete(item_name)
+    canvas.delete(item_name+'_txt')
+    if item_name in blocks_dict:
+        blocks_dict.pop(item_name)
+    elif item_name in arcs_dict:
+        arcs_dict.pop(item_name)
+        
+    item_selection.set(None)
+    item_selection_label.config(text='')
     
 # write the code of the current system
 def write_code():
@@ -156,7 +146,7 @@ def write_code():
     code_arcs = "#CONNECTIONS\n"
     
     for b in blocks_dict:
-        # TODO: full data, init and t
+        # TODO: (issue #23) full data, init and t
         code = "m."+b+" = pyo.Block()\n"
         code = code + "data = {}\n"
         code = code + "init = {}\n"
@@ -179,6 +169,11 @@ def write_code():
 
 # === TKINTER WINDOW === 
 
+devices_dict = read_devices()
+last_block_id = {d: 0 for d in devices_dict}
+init_pos = [0,0]
+is_drawing = False
+
 window = tk.Tk()
 
 window.title("System")
@@ -190,40 +185,34 @@ window.resizable(False,False)
 frame_tool_left = tk.Frame(window, width=400, height=100)
 frame_tool_left.grid(row=0, column=0, sticky=tk.NW)
 
+# -- select
+sel_btn = tk.Button(frame_tool_left, text='Select', command=lambda: set_current_canvas_function('sel'))
+sel_btn.grid(row=0,column=0)
+
 # -- arcs
 arc_btn = tk.Button(frame_tool_left, text='Arc', command=lambda: set_current_canvas_function('arc'))
-arc_btn.grid(row=0,column=0)
+arc_btn.grid(row=0,column=1)
 
 # -- devices
 device_btn = tk.Button(frame_tool_left, text='Device', command=lambda: set_current_canvas_function('block'))
-device_btn.grid(row=0,column=1)
+device_btn.grid(row=0,column=2)
     
 devices_menu_selection = tk.StringVar()
 devices_menu_selection.set(list(devices_dict.keys())[0])
 devices_menu = tk.OptionMenu(frame_tool_left, devices_menu_selection, *list(devices_dict.keys()))
-devices_menu.grid(row=0,column=2)
+devices_menu.grid(row=0,column=3)
 
-# Delete
+# Selection and Delete
 frame_tool_right = tk.Frame(window, width=400, height=100)
 frame_tool_right.grid(row=0, column=0, sticky=tk.NE)
 
-# -- blocks
-delete_menu_selection = tk.StringVar()
-delete_menu = tk.OptionMenu(frame_tool_right, delete_menu_selection, *blocks_list)
-delete_menu.grid(row=0,column=0)
+item_selection = tk.StringVar()
+item_selection_label = tk.Label(frame_tool_right, text='')
+item_selection_label.grid(row=0,column=0)
 
-del_btn = tk.Button(frame_tool_right, text='Delete Block', width=8,
-                    command=lambda: delete_block(delete_menu_selection.get()))
+del_btn = tk.Button(frame_tool_right, text='Delete',
+                    command=lambda: delete_item(item_selection.get()))
 del_btn.grid(row=0,column=1)
-
-# -- arcs
-delete_menu2_selection = tk.StringVar()
-delete_menu2 = tk.OptionMenu(frame_tool_right, delete_menu2_selection, *arcs_list)
-delete_menu2.grid(row=1,column=0)
-
-del_btn2 = tk.Button(frame_tool_right, text='Delete Arc', width=8,
-                     command=lambda: delete_arc(delete_menu2_selection.get()))
-del_btn2.grid(row=1,column=1)
 
 # CANVAS
 canvas_frame = tk.Frame(window, width=800, height=600)
