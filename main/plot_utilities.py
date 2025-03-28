7656e787e2066f179c0c14155de680ff57fc8137
sergicosta <sergi.costa.dilme@upc.edu>
2025-03-28 12:50:53 +0000

    docs(utilities): spiderplot doc

--- main/plot_utilities.py
+++ main/plot_utilities.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# data: pd.DataFrame() with the form {'x1':{x1_sensitivity_vals...}, 'x2':{x2vals...}, ... 'y':{yvals}}
+# y: y column name as str
+# base_idx: index of the row that contains the base case as int (default: 0)
 def spiderplot(data, y, base_idx=0):
     data_pu = data/data.iloc[base_idx]
     columns = data.drop(y,axis=1).columns
